51ae08c038d50ecfb145dfce0a6272efef7b80db
Michael Steinbaugh <mike@steinbaugh.com>
2020-10-06 15:29:00 -0400

    Improve camel case naming in functions

--- R/imports.R
+++ R/imports.R
@@ -1,12 +1,15 @@
 #' @importClassesFrom GenomicRanges GRanges
 #' @importClassesFrom S4Vectors DataFrame Vector
 #'
+#' @importFrom AcidBase pasteURL
 #' @importFrom GenomicRanges GRanges
 #' @importFrom IRanges IRanges
+#' @importFrom RCurl getURL
 #' @importFrom S4Vectors mcols mcols<-
 #' @importFrom goalie assert bapply hasLength hasNoDuplicates hasRows isAny
 #'   isCharacter isFlag isInt isString isSubset validate
 #' @importFrom methods as is setClass setGeneric setValidity
-#' @importFrom pipette cacheURL import
+#' @importFrom pipette cacheURL getURLDirList import
+#' @importFrom stringr boundary str_extract str_split_fixed
 #' @importFrom utils packageName
 NULL
