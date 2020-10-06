1f0082aca42d259aba9d99b2b999532fe86d6300
Michael Steinbaugh <mike@steinbaugh.com>
2020-10-06 15:53:16 -0400

    Update NAMESPACE

--- R/imports.R
+++ R/imports.R
@@ -2,7 +2,8 @@
 #' @importClassesFrom S4Vectors DataFrame Vector
 #'
 #' @importFrom AcidBase forceDetach pasteURL
-#' @importFrom AnnotationHub AnnotationHub
+#' @importFrom AnnotationDbi select
+#' @importFrom AnnotationHub AnnotationHub query snapshotDate
 #' @importFrom BiocParallel bplapply
 #' @importFrom GenomicRanges GRanges
 #' @importFrom IRanges IRanges
@@ -11,11 +12,11 @@
 #'   as.data.frame as.list complete.cases cor decode do.call expand expand.grid
 #'   head lapply match mcols mcols<- merge metadata metadata<- na.omit order
 #'   setdiff split summary t tail
-#' @importFrom cli cli_alert
+#' @importFrom cli cli_alert cli_alert_info cli_alert_warning cli_dl
 #' @importFrom goalie assert bapply hasLength hasNoDuplicates hasRows isAny
 #'   isCharacter isFlag isInt isString isSubset validate
-#' @importFrom methods as is setClass setGeneric setValidity
+#' @importFrom methods as is new setClass setGeneric setValidity
 #' @importFrom pipette cacheURL getURLDirList import
 #' @importFrom stringr boundary str_extract str_split_fixed
-#' @importFrom utils packageName
+#' @importFrom utils packageName packageVersion
 NULL
