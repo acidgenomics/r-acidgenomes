8bff8d2fbae21bfd992b0b6ae810c3f869676567
Michael Steinbaugh <mike@steinbaugh.com>
2020-10-06 13:20:57 -0400

    Migrate more code from basejump

--- R/imports.R
+++ R/imports.R
@@ -1,3 +1,8 @@
-#' @importFrom goalie bapply hasNoDuplicates hasRows isAny isSubset validate
+#' @importClassesFrom S4Vectors DataFrame
+#'
+#' @importFrom goalie assert bapply hasNoDuplicates hasRows isAny isCharacter
+#'   isFlag isInt isString isSubset validate
 #' @importFrom methods setClass setGeneric setValidity
+#' @importFrom pipette cacheURL import
+#' @importFrom utils packageName
 NULL
