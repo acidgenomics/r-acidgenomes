c8c8b0f93dc8adbdcb5d58a02cd2f4cd4f4b747d
Michael Steinbaugh <mike@steinbaugh.com>
2020-10-06 18:23:43 -0400

    Update NAMESPACE

--- R/imports.R
+++ R/imports.R
@@ -1,7 +1,8 @@
 #' @importClassesFrom GenomicRanges GRanges
 #' @importClassesFrom S4Vectors DataFrame Vector
 #'
-#' @importFrom AcidBase forceDetach pasteURL
+#' @importFrom AcidBase forceDetach matchArgsToDoCall pasteURL requireNamespaces
+#' @importFrom AcidPlyr leftJoin
 #' @importFrom AnnotationDbi select
 #' @importFrom AnnotationHub AnnotationHub query snapshotDate
 #' @importFrom BiocParallel bplapply
@@ -15,12 +16,15 @@
 #' @importFrom SummarizedExperiment SummarizedExperiment assay assay<-
 #'   assayNames assayNames<- assays assays<- colData colData<- rowData rowData<-
 #'   rowRanges rowRanges<-
-#' @importFrom cli cli_alert cli_alert_info cli_alert_warning cli_dl
-#' @importFrom goalie areSetEqual assert bapply hasInternet hasLength hasNoDuplicates
-#'   hasRows isAny isCharacter isFlag isInt isString isSubset validate
+#' @importFrom cli cli_alert cli_alert_info cli_alert_success cli_alert_warning
+#'   cli_div cli_dl cli_end cli_li cli_text cli_ul cli_verbatim
+#' @importFrom goalie areSetEqual assert bapply hasDuplicates hasInternet
+#'   hasLength hasNoDuplicates hasNames hasRows isAny isCharacter isFlag isInt
+#'   isMatchingRegex isString isSubset validate
 #' @importFrom methods as is new setClass setGeneric setValidity
 #' @importFrom pipette as.SummarizedExperiment cacheURL getURLDirList import
-#' @importFrom stringr boundary str_extract str_split_fixed
+#'   removeNA sanitizeNA
+#' @importFrom stringr boundary str_extract str_match str_split_fixed
 #' @importFrom syntactic camelCase
 #' @importFrom utils packageName packageVersion
 NULL
