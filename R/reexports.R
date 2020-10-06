0924a673b5041fab9a58985a81169c4b99522ec1
Michael Steinbaugh <mike@steinbaugh.com>
2020-10-06 19:28:36 -0400

    Update NAMESPACE

--- R/reexports.R
+++ R/reexports.R
@@ -1,13 +1,3 @@
 #' @importFrom AcidBase pasteURL
 #' @export
 AcidBase::pasteURL
-
-
-
-#' @importFrom BiocGenerics organism
-#' @export
-BiocGenerics::organism
-
-#' @importFrom BiocGenerics organism<-
-#' @export
-BiocGenerics::`organism<-`
