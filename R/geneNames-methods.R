fb0aceaf3bdf9041bb6f362270bc98af507028f6
Michael Steinbaugh <mike@steinbaugh.com>
2020-10-06 14:00:25 -0400

    Migrate functions from basejump

---
+++ R/geneNames-methods.R
@@ -0,0 +1,63 @@
+#' @name geneNames
+#' @inherit AcidGenerics::geneNames
+#' @note Updated 2019-07-28.
+#'
+#' @inheritParams AcidRoxygen::params
+#' @param ... Additional arguments.
+#'
+#' @examples
+#' data(RangedSummarizedExperiment, package = "AcidTest")
+#' rse <- RangedSummarizedExperiment
+#'
+#' ## SummarizedExperiment ====
+#' head(geneNames(rse))
+NULL
+
+
+
+#' @rdname geneNames
+#' @name geneNames
+#' @importFrom AcidGenerics geneNames
+#' @usage geneNames(object, ...)
+#' @export
+NULL
+
+
+
+## Updated 2019-07-22.
+`geneNames,Vector` <-  # nolint
+    function(object) {
+        Gene2Symbol(object, format = "makeUnique")[["geneName"]]
+    }
+
+
+
+## Updated 2019-07-22.
+`geneNames,GRanges` <-  # nolint
+    `geneNames,Vector`
+
+
+
+#' @rdname geneNames
+#' @export
+setMethod(
+    f = "geneNames",
+    signature = signature("GRanges"),
+    definition = `geneNames,GRanges`
+)
+
+
+
+## Updated 2019-07-22.
+`geneNames,SummarizedExperiment` <-  # nolint
+    `geneNames,Vector`
+
+
+
+#' @rdname geneNames
+#' @export
+setMethod(
+    f = "geneNames",
+    signature = signature("SummarizedExperiment"),
+    definition = `geneNames,SummarizedExperiment`
+)
