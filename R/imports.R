c9560d3afe210c2fb58236a29cc017463e8cab5a
Michael Steinbaugh <mike@steinbaugh.com>
2020-10-06 12:34:49 -0400

    Draft commit of genome handling

--- R/imports.R
+++ R/imports.R
@@ -1,2 +1,3 @@
-#' @importFrom methods setClass setValidity
+#' @importFrom goalie bapply hasNoDuplicates hasRows isAny isSubset validate
+#' @importFrom methods setClass setGeneric setValidity
 NULL
