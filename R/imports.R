56d969c8a4b802d0f71b8bc30c6a657090a3eaa8
Michael Steinbaugh <mike@steinbaugh.com>
2020-10-06 14:19:58 -0400

    Update NAMESPACE to get emptyRanges to work

--- R/imports.R
+++ R/imports.R
@@ -1,9 +1,12 @@
 #' @importClassesFrom GenomicRanges GRanges
 #' @importClassesFrom S4Vectors DataFrame Vector
 #'
-#' @importFrom goalie assert bapply hasNoDuplicates hasRows isAny isCharacter
-#'   isFlag isInt isString isSubset validate
-#' @importFrom methods setClass setGeneric setValidity
+#' @importFrom GenomicRanges GRanges
+#' @importFrom IRanges IRanges
+#' @importFrom S4Vectors mcols mcols<-
+#' @importFrom goalie assert bapply hasLength hasNoDuplicates hasRows isAny
+#'   isCharacter isFlag isInt isString isSubset validate
+#' @importFrom methods as is setClass setGeneric setValidity
 #' @importFrom pipette cacheURL import
 #' @importFrom utils packageName
 NULL
