ca74d15562cad1ef49a61564c8cc55fb164df612
Michael Steinbaugh <mike@steinbaugh.com>
2020-12-10 10:33:46 -0500

    Work toward working draft of genome downloader functions

--- R/imports.R
+++ R/imports.R
@@ -2,8 +2,8 @@
 #' @importClassesFrom IRanges DataFrameList
 #' @importClassesFrom S4Vectors DataFrame Vector
 #'
-#' @importFrom AcidBase forceDetach matchArgsToDoCall pasteURL printString
-#'   requireNamespaces showSlotInfo
+#' @importFrom AcidBase forceDetach initDir matchArgsToDoCall pasteURL
+#'   printString requireNamespaces showSlotInfo
 #' @importFrom AcidPlyr leftJoin
 #' @importFrom AnnotationDbi select
 #' @importFrom AnnotationHub AnnotationHub query snapshotDate
@@ -14,7 +14,7 @@
 #'   do.call expand head lapply match mcols mcols<- metadata metadata<- order
 #'   setdiff split summary tail
 #' @importFrom SummarizedExperiment rowData rowRanges
-#' @importFrom cli cli_alert cli_alert_info cli_alert_warning
+#' @importFrom cli cli_alert cli_alert_info cli_alert_success cli_alert_warning
 #'   cli_div cli_dl cli_end
 #' @importFrom ensembldb ensemblVersion genes transcripts
 #' @importFrom goalie areDisjointSets areSetEqual assert bapply hasDuplicates
@@ -22,8 +22,8 @@
 #'   isCharacter isFlag isInt isMatchingRegex isString isSubset validate
 #' @importFrom methods as is new setClass setGeneric setMethod setValidity
 #'   signature
-#' @importFrom pipette as_tibble as.SummarizedExperiment cacheURL getURL
-#'   getURLDirList import removeNA sanitizeNA
+#' @importFrom pipette as_tibble as.SummarizedExperiment cacheURL download
+#'   getURL getURLDirList import removeNA sanitizeNA
 #' @importFrom stringr boundary str_extract str_match str_split_fixed
 #' @importFrom syntactic camelCase makeNames
 #' @importFrom utils capture.output packageName packageVersion
