e8583d607bd114632fa3294082f12332383835ad
Michael Steinbaugh <mike@steinbaugh.com>
2021-01-06 21:05:38 -0500

    Update NAMESPACE

--- R/imports.R
+++ R/imports.R
@@ -4,7 +4,7 @@
 #'
 #' @importFrom AcidBase compress download forceDetach initDir matchArgsToDoCall
 #'   pasteURL printString requireNamespaces showSlotInfo
-#' @importFrom AcidCLI alert alertInfo alertWarning dl
+#' @importFrom AcidCLI alert alertInfo alertSuccess alertWarning dl
 #' @importFrom AcidGenerics %in% as.data.frame complete.cases decode do.call
 #'   expand head lapply match mcols mcols<- metadata metadata<- order setdiff
 #'   split tail
