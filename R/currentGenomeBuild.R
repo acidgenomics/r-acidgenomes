2e230adfa2674e8f3858bbd6bf7e7638b6f29e8e
Michael Steinbaugh <mike@steinbaugh.com>
2021-01-08 15:43:33 -0500

    Rework RefSeq approach

--- R/currentGenomeBuild.R
+++ R/currentGenomeBuild.R
@@ -79,69 +79,6 @@ currentGencodeBuild <- function(organism) {
 
 
 
-#' Get the RefSeq organism directory structure on the FTP server
-#'
-#' @note Updated 2021-01-08.
-#' @noRd
-.getRefSeqGenomeURL <- function(
-    organism,
-    taxonomicGroup = NULL,
-    quiet = FALSE
-) {
-    assert(
-        isOrganism(organism),
-        isString(taxonomicGroup, nullOK = TRUE),
-        isFlag(quiet)
-    )
-    if (isFALSE(quiet)) {
-        alert(sprintf(
-            "Locating {.emph %s} genome on RefSeq FTP server.",
-            organism
-        ))
-    }
-    baseURL <- "ftp://ftp.ncbi.nlm.nih.gov/genomes/refseq"
-    if (is.null(taxonomicGroup)) {
-        taxonomicGroups <- getURLDirList(url = baseURL)
-        keep <- grepl(pattern = "^[a-z_]+$", x = taxonomicGroups)
-        taxonomicGroups <- sort(taxonomicGroups[keep])
-        list <- bplapply(
-            X = taxonomicGroups,
-            baseURL = baseURL,
-            FUN = function(taxonomicGroup, baseURL) {
-                url <- pasteURL(baseURL, taxonomicGroup)
-                x <- getURLDirList(url = url)
-                keep <- grepl(pattern = "^[A-Z][a-z]+_[a-z]+$", x = x)
-                x <- sort(x[keep])
-                x
-            }
-        )
-        names(list) <- taxonomicGroups
-        match <- vapply(
-            X = list,
-            organism = gsub(pattern = " ", replacement = "_", x = organism),
-            FUN = function(strings, organism) {
-                isSubset(x = organism, y = strings)
-            },
-            FUN.VALUE = logical(1L),
-            USE.NAMES = TRUE
-        )
-        taxonomicGroup <- names(match)[match]
-        assert(isString(taxonomicGroup))
-    }
-    url <- pasteURL(
-        baseURL,
-        taxonomicGroup,
-        gsub(pattern = " ", replacement = "_", x = organism)
-    )
-    assert(url.exists(url))
-    if (isFALSE(quiet)) {
-        dl(c("URL" = url))
-    }
-    url
-}
-
-
-
 ## Alternate approach using URL only:
 ## https://ftp.ncbi.nlm.nih.gov/genomes/refseq/<taxonomic_group>/<organism>/
 ##     latest_assembly_versions/
@@ -161,20 +98,9 @@ currentRefSeqGenomeBuild <- function(
         taxonomicGroup = taxonomicGroup,
         quiet = TRUE
     )
-    url <- pasteURL(baseURL, "assembly_summary.txt")
-    df <- import(
-        file = url,
-        format = "tsv",
-        colnames = FALSE,
-        skip = 2L,
-        quiet = TRUE
-    )
-    vec <- as.character(df[1L, , drop = TRUE])
-    if (!isSubset(organism, vec)) {
-        stop("Invalid organism.")
-    }
-    out <- vec[[1L]]
-    assert(isString(out))
+    summary <- .getRefSeqAssemblySummary(baseURL = baseURL)
+    assert(isSubset("assembly_accession", names(summary)))
+    out <- summary[["assembly_accession"]]
     out
 }
 
