9559ec571a82bcd83581847f7bcd6b3babe0bca3
Michael Steinbaugh <mike@steinbaugh.com>
2021-01-08 15:24:27 -0500

    Add internal RefSeq FTP utility functions

---
+++ R/internal-RefSeq.R
@@ -0,0 +1,86 @@
+#' Get the RefSeq assembly metadata
+#'
+#' @note Updated 2021-01-08.
+#' @noRd
+#'
+#' @return Named `character`.
+.getRefSeqAssemblySummary <-
+    function(baseURL) {
+        lines <- import(
+            file = pasteURL(baseURL, "assembly_summary.txt"),
+            format = "lines",
+            skip = 1L,
+            quiet = TRUE
+        )
+        names <- strsplit(
+            x = sub(pattern = "^#\\s", replacement = "", x = lines[[1L]]),
+            split = "\\t"
+        )[[1L]]
+        values <- strsplit(x = lines[[2L]], split = "\\t")[[1L]]
+        x <- as.character(values[seq_len(20L)])
+        names(x) <- names[seq_len(20L)]
+        x <- x[nzchar(x)]
+        x
+    }
+
+
+
+#' Get the RefSeq organism directory structure on the FTP server
+#'
+#' @note Updated 2021-01-08.
+#' @noRd
+.getRefSeqGenomeURL <- function(
+    organism,
+    taxonomicGroup = NULL,
+    quiet = FALSE
+) {
+    assert(
+        isOrganism(organism),
+        isString(taxonomicGroup, nullOK = TRUE),
+        isFlag(quiet)
+    )
+    if (isFALSE(quiet)) {
+        alert(sprintf(
+            "Locating {.emph %s} genome on RefSeq FTP server.",
+            organism
+        ))
+    }
+    baseURL <- "ftp://ftp.ncbi.nlm.nih.gov/genomes/refseq"
+    if (is.null(taxonomicGroup)) {
+        taxonomicGroups <- getURLDirList(url = baseURL)
+        keep <- grepl(pattern = "^[a-z_]+$", x = taxonomicGroups)
+        taxonomicGroups <- sort(taxonomicGroups[keep])
+        list <- bplapply(
+            X = taxonomicGroups,
+            baseURL = baseURL,
+            FUN = function(taxonomicGroup, baseURL) {
+                url <- pasteURL(baseURL, taxonomicGroup)
+                x <- getURLDirList(url = url)
+                keep <- grepl(pattern = "^[A-Z][a-z]+_[a-z]+$", x = x)
+                x <- sort(x[keep])
+                x
+            }
+        )
+        names(list) <- taxonomicGroups
+        match <- vapply(
+            X = list,
+            organism = gsub(pattern = " ", replacement = "_", x = organism),
+            FUN = function(strings, organism) {
+                isSubset(x = organism, y = strings)
+            },
+            FUN.VALUE = logical(1L),
+            USE.NAMES = TRUE
+        )
+        taxonomicGroup <- names(match)[match]
+        assert(isString(taxonomicGroup))
+    }
+    url <- pasteURL(
+        baseURL,
+        taxonomicGroup,
+        gsub(pattern = " ", replacement = "_", x = organism)
+    )
+    if (isFALSE(quiet)) {
+        dl(c("URL" = url))
+    }
+    url
+}
