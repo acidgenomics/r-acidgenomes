056d3b0f5a23a7abc71aa986167f7831271a0ce3
Michael Steinbaugh <mike@steinbaugh.com>
2021-01-10 13:34:28 -0500

    Draft update

--- R/internal-GenomicFeatures.R
+++ R/internal-GenomicFeatures.R
@@ -47,4 +47,35 @@
 
 
 
-## nocov end
+#' Make GRanges from TxDb object
+#'
+#' @note Updated 2021-01-10.
+#' @noRd
+#'
+#' @return `GRanges`.
+.makeGRangesFromTxDb <- function(
+    object,
+    level = c("genes", "transcripts")
+) {
+    assert(is(object, "TxDb"))
+    level <- match.arg(level)
+    args <- list("x" = object)
+    ## FIXME THIS RETURNS INCORRECTLY FOR DROSOPHILA.
+    ## FIXME NEED TO PARSE THE GTF DIRECTLY TO GET METADATA...
+    keys <- AnnotationDbi::columns(object)
+    switch(
+        EXPR = level,
+        "genes" = {
+            fun <- GenomicFeatures::genes
+            columns <- "gene_id"
+        },
+        "transcripts" = {
+            fun <- GenomicFeatures::transcripts
+            columns <- c("tx_id", "tx_name", "gene_id")
+        }
+    )
+    args[["columns"]] <- columns
+    gr <- do.call(what = fun, args = args)
+    assert(is(gr, "GRanges"))
+    gr
+}
