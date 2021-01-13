2ee609092914632cab0cf197e3cc201d426818f3
Michael Steinbaugh <mike@steinbaugh.com>
2021-01-13 08:24:09 -0500

    Draft updates to GFF/GTF utility parsers

---
+++ R/internal-GRanges-draft.R
@@ -0,0 +1,23 @@
+#' Get RefSeq seqlengths
+#'
+#' @note Updated 2021-01-13.
+#' @noRd
+#'
+#' @rdname releaseURL `character(1)`.
+#'   RefSeq genome release URL
+#'   (e.g.
+#'   ).
+#'
+#' @seealso `tximeta:::gtf2RefSeq`.
+.getRefSeqSeqinfo <- function(
+    releaseURL,
+    genome = NULL
+) {
+    ## ATTEMPT TO DEFINE THE GENOME FROM THE FILE NAME.
+    if (is.null(genome)) {
+
+    }
+
+    "assembly_report"
+
+}
