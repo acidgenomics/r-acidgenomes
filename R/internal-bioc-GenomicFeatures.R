0d23b94119496b1b1b154571274d9a873cbb30c1
Michael Steinbaugh <mike@steinbaugh.com>
2021-01-13 17:50:47 -0500

    Draft update, switching machines

--- R/internal-bioc-GenomicFeatures.R
+++ R/internal-bioc-GenomicFeatures.R
@@ -10,8 +10,17 @@
 #' ```
 #'
 #' @seealso
+#' - `GenomicFeatures::makeTxDbFromGFF()`.
+#' - `GenomicFeatures:::compareTxDbs()`.
+#' - [`makeTxDbFromGFF()` unit tests](https://github.com/Bioconductor/GenomicFeatures/blob/master/inst/unitTests/test_makeTxDbFromGFF.R)
+#' - [BSgenome.Hsapiens.NCBI.GRCh38](https://bioconductor.org/packages/BSgenome.Hsapiens.NCBI.GRCh38/).
+#' - [TxDb.Hsapiens.UCSC.hg38.knownGene](https://bioconductor.org/packages/TxDb.Hsapiens.UCSC.hg38.knownGene/).
+
 #' - https://stackoverflow.com/questions/38603668
 #' - https://stackoverflow.com/questions/16517795
+#' - https://github.com/Bioconductor/GenomicFeatures/blob/master/
+#'       R/makeTxDbFromGRanges.R
+#' - https://github.com/Bioconductor/GenomicFeatures/issues/26
 #'
 #' @noRd
 .makeTxDbFromGFF <- function(file) {
@@ -22,12 +31,29 @@
     ))
     requireNamespaces("GenomicFeatures")
     assert(isAFile(file))
+    ## RefSeq GCF_000001405.39_GRCh38.p13 input is hitting this error:
+    ## some CDS cannot be mapped to an exon
+    ## https://github.com/Bioconductor/GenomicFeatures/issues/26
     txdb <- withCallingHandlers(expr = {
         tryCatch(
             expr = {
-                GenomicFeatures::makeTxDbFromGFF(file)
+                GenomicFeatures::makeTxDbFromGFF(
+                    file = file,
+                    format = "auto",
+                    ## > dataSource = NA,
+                    organism = "FIXME",
+                    ## > taxonomyId = NA,
+                    ## > circ_seqs = NULL,
+                    chrominfo = "FIXME"
+                    ## This could be useful for future genome builds with miRs.
+                    ## > miRBaseBuild = NA,
+                    ## > metadata = NULL,
+                    ## This can help override feature name ("e.g. GeneID") used.
+                    ## > dbxrefTag = "GeneID")
+                )
             },
             error = function(e) {
+                warning(e)
                 stop(paste(
                     "Failed to make TxDb using ",
                     "'GenomicFeatures::makeTxDbFromGFF()'",
