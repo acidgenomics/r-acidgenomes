a6964e8e2c54ab52fbf9915abc57adfc627dff69
Michael Steinbaugh <mike@steinbaugh.com>
2021-01-14 11:06:59 -0500

    Update NAMESPACE

--- R/imports.R
+++ R/imports.R
@@ -22,7 +22,7 @@
 #' @importFrom goalie allAreFiles allAreURLs areDisjointSets areSetEqual assert
 #'   bapply hasDuplicates hasInternet hasLength hasNoDuplicates hasNames
 #'   hasRownames hasRows isADir isAFile isAURL isAny isCharacter isFlag isInt
-#'   isMatchingRegex isOrganism isString isSubset validate
+#'   isMatchingFixed isMatchingRegex isOrganism isString isSubset validate
 #' @importFrom httr GET content content_type stop_for_status
 #' @importFrom jsonlite fromJSON toJSON
 #' @importFrom methods as is new setClass setGeneric setMethod setValidity
