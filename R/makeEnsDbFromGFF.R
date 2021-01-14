54331f61677bbc8b9818ab4f2269189f866adadf
Michael Steinbaugh <mike@steinbaugh.com>
2021-01-14 09:39:21 -0500

    Export makeEnsDbFromGFF function

--- R/makeEnsDbFromGFF.R
+++ R/makeEnsDbFromGFF.R
@@ -3,15 +3,34 @@
 #' @export
 #' @note Updated 2021-01-14.
 #'
+#' @inheritParams AcidRoxygen::params
+#' @inheritParams params
+#'
 #' @return `EnsDb`.
 #'
 #' @examples
 #' file <- pasteURL(AcidGenomesTestsURL, "ensembl.gtf")
-#' edb <- makeEnsDbFromGFF(file = file)
-makeEnsDbFromGFF <- function(file) {
+#' edb <- makeEnsDbFromGFF(
+#'     file = file,
+#'     organism = "Homo sapiens",
+#'     genomeBuild = "GRCh38",
+#'     release = 100L
+#' )
+makeEnsDbFromGFF <- function(
+    file,
+    organism = NULL,
+    genomeBuild = NULL,
+    release = NULL
+) {
+    assert(
+        isString(file),
+        isString(organism, nullOK = TRUE),
+        isString(genomeBuild, nullOK = TRUE),
+        isInt(release, nullOK = TRUE)
+    )
     requireNamespaces("ensembldb")
     alert(sprintf("Making {.var %s} from {.file %s}.", "EnsDb", file))
-    tmpfile <- .cache(file)
+    tmpfile <- .cacheIt(file)
     ext <- fileExt(file)
     args <- list("outfile" = tempfile())
     ## e.g. 11f4650926be_Homo_sapiens.GRCh38.102.gtf.gz
@@ -26,13 +45,29 @@ makeEnsDbFromGFF <- function(file) {
     if (isTRUE(grepl(pattern = pattern, x = basename(file)))) {
         alert("Detecting genome metadata from file name.")
         match <- str_match(string = basename(file), pattern = pattern)
-        args[["organism"]] <-
-            gsub(pattern = "_", replacement = " ", x = match[1L, 3L])
-        args[["genomeVersion"]] <- match[1L, 4L]
-        args[["version"]] <- as.integer(match[1L, 5L])
-    } else {
-        alertWarning("Failed to match genome metadata from file name.")
+        organism <- gsub(pattern = "_", replacement = " ", x = match[1L, 3L])
+        genomeBuild <- match[1L, 4L]
+        release <- as.integer(match[1L, 5L])
+    } else if (any(is.null(organism), is.null(genomeBuild), is.null(release))) {
+        ## nocov start
+        stop(sprintf(
+            paste(
+                "Failed to match genome metadata from file name ('%s').",
+                "Define values manually: %s."
+            ),
+            file,
+            toString(c("organism", "genomeBuild", "release"))
+        ))
+        ## nocov end
     }
+    args <- append(
+        x = args,
+        values = list(
+            "organism" = organism,
+            "genomeVersion" = genomeBuild,
+            "version" = release
+        )
+    )
     if (grepl(pattern = "gff", x = ext, ignore.case = TRUE)) {
         what <- ensembldb::ensDbFromGff
         args <- append(x = args, values = list("gff" = tmpfile))
@@ -43,7 +78,9 @@ makeEnsDbFromGFF <- function(file) {
         stop("Unsupported file extension.")  # nocov
     }
     suppressWarnings({
-        sqlite <- do.call(what = what, args = args)
+        suppressMessages({
+            sqlite <- do.call(what = what, args = args)
+        })
     })
     edb <- ensembldb::EnsDb(x = sqlite)
     assert(is(edb, "EnsDb"))
