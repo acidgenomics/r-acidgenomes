8912dbc3556446615db1c49ed4db01e1770b72eb
Michael Steinbaugh <mike@steinbaugh.com>
2021-01-14 09:18:57 -0500

    Simplify internal caching

--- R/internal-bioc-BiocFileCache.R
+++ R/internal-bioc-BiocFileCache.R
@@ -1,14 +1,23 @@
 #' Dynamically cache a URL into package or return local file
 #'
 #' @note Updated 2021-01-14.
+#' @note R won't let you name the function `.cache`.
 #' @noRd
-.cache <- function(file) {
+#'
+#' @return `character(1)`.
+#'   Local file path
+#'
+#' @examples
+#' file <- pasteURL(AcidGenomesTestsURL, "ensembl.gtf")
+#' tmpfile <- .cacheIt(file)
+#' print(tmpfile)
+.cacheIt <- function(file) {
     assert(isString(file))
     if (isAURL(file)) {
         x <- cacheURL(url = file, pkg = packageName())
     } else {
         x <- file
     }
-    assert(isAFile(tmpfile))
+    assert(isAFile(x))
     x
 }
