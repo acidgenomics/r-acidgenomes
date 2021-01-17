d046f9e0a3b816b0d163de4b9b65801d06e00d25
Michael Steinbaugh <mike@steinbaugh.com>
2021-01-17 09:25:06 -0500

    Update timestamp

--- R/Gene2Symbol-methods.R
+++ R/Gene2Symbol-methods.R
@@ -6,7 +6,7 @@
 #'   the documentation for approaches that deal with this issue.
 #' @note For the `format` argument, note that "long" was used instead of
 #'   "unmodified" prior to v0.10.10.
-#' @note Updated 2021-01-06.
+#' @note Updated 2021-01-17.
 #'
 #' @inheritParams AcidRoxygen::params
 #' @param format `character(1)`.
