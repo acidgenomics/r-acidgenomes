5ebf122d34b9beee69892e7ecdfbd59236747929
Michael Steinbaugh <mike@steinbaugh.com>
2021-01-18 08:45:23 -0500

    Rework internal GRanges functions

--- R/internal-GRanges.R
+++ R/internal-GRanges.R
@@ -1,278 +1,4 @@
-#' Add broad class annotations
-#'
-#' @note Updated 2020-10-05.
-#' @noRd
-.addBroadClass <- function(object) {
-    assert(is(object, "GRanges"))
-    mcols(object)[["broadClass"]] <- Rle(.broadClass(object))
-    object
-}
-
-
-
-#' Add Ensembl gene synonyms
-#'
-#' @note Updated 2020-10-05.
-#' @noRd
-.addGeneSynonyms <- function(object) {
-    assert(is(object, "GRanges"))
-    mcols <- mcols(object)
-    assert(isSubset("geneId", colnames(mcols)))
-    if (!any(grepl(pattern = "^ENS", x = mcols[["geneId"]]))) {
-        return(object)
-    }
-    organism <- organism(object)
-    if (!isSubset(organism, eval(formals(geneSynonyms)[["organism"]]))) {
-        return(object)
-    }
-    alert(sprintf(
-        "Adding gene synonyms to {.var %s} column.",
-        "geneSynonyms"
-    ))
-    synonyms <- geneSynonyms(organism = organism, return = "DataFrame")
-    ## FIXME RETHINK THIS.
-    assert(identical(c("geneId", "geneSynonyms"), colnames(synonyms)))
-    mcols <- leftJoin(x = mcols, y = synonyms, by = "geneId")
-    mcols(object) <- mcols
-    object
-}
-
-
-
-#' Add the transcript version
-#'
-#' Append the transcript version to the identifier (e.g. ENST00000000233.10).
-#'
-#' @note Updated 2021-01-06.
-#' @noRd
-.addTxVersion <- function(object) {
-    alert("Adding version to transcript identifiers.")
-    mcolnames <- colnames(mcols(object))
-    assert(
-        is(object, "GRanges"),
-        identical(metadata(object)[["level"]], "transcripts"),
-        isSubset("txId", mcolnames)
-    )
-    if (isSubset("txIdVersion", mcolnames)) {
-        ## `makeGRangesFromEnsembl()` output via ensembldb.
-        id <- mcols(object)[["txIdVersion"]]
-    } else if (isSubset("txVersion", mcolnames)) {
-        ## `makeGRangesFromGFF()` output.
-        id <- mcols(object)[["txId"]]
-        version <- mcols(object)[["txVersion"]]
-        id <- Rle(paste(id, version, sep = "."))
-    } else {
-        stop("Failed to locate transcript version metadata.")  # nolint
-    }
-    mcols(object)[["txId"]] <- id
-    ## Note that names are set by `.makeGRanges()`.
-    object
-}
-
-
-
-#' Apply broad class definitions
-#'
-#' This function is intended to work rowwise on the GRanges mcols.
-#'
-#' @section Mitochondrial genes:
-#' Mitochondrial gene matching depends on the genome.
-#' This is important in particular for single-cell RNA-seq.
-#'
-#' - *H. sapiens*: "MT-" gene name.
-#' - *M. musculus*: "mt-" gene name.
-#' - *D. melanogaster*: "mt:" gene name.
-#' - *C. elegans*: Can't match by gene name. Match by "MtDNA" chromosome.
-#'   Alternatively, can match using "MTCE" sequence name (parent clone).
-#'   https://www.wormbase.org/species/c_elegans/clone/MTCE
-#'
-#' Note that this might not be perfect for other genomes, so consider
-#' atttempting to improve support here in a future update.
-#'
-#' @seealso Can use `dplyr::case_when()` instead, which allows for a rowwise
-#'   vectorized if/else call stack.
-#'
-#' @note Can return `NA_character_` here instead. Keeping this as "other", to
-#'   main consistency with previous data sets. Also note that `NA` can behave
-#'   inconsistently in plotting engines.
-#' @note Updated 2020-01-20.
-#'
-#' @author Rory Kirchner, Michael Steinbaugh
-#' @noRd
-#'
-#' @param x `list`.
-#'   List returned via apply call using `MARGIN = 1`.
-#'
-#' @return `character(1)`.
-.applyBroadClass <- function(x) {
-    if (
-        isTRUE(grepl(
-            pattern = "^MT",
-            x = x[["chromosome"]],
-            ignore.case = TRUE
-        )) ||
-        isTRUE(grepl(
-            pattern = "^mt[\\:\\-]",
-            x = x[["geneName"]],
-            ignore.case = TRUE
-        ))
-    ) {
-        "mito"
-    } else if (x[["biotype"]] == "protein_coding") {
-        "coding"
-    } else if (
-        x[["biotype"]] %in% c(
-            "known_ncrna",
-            "lincRNA",
-            "non_coding"
-        )
-    ) {
-        "noncoding"
-    } else if (
-        isTRUE(grepl(
-            pattern = "pseudo",
-            x = x[["biotype"]],
-            ignore.case = TRUE
-        ))
-    ) {
-        "pseudo"
-    } else if (
-        x[["biotype"]] %in% c(
-            "miRNA",
-            "misc_RNA",
-            "ribozyme",
-            "rRNA",
-            "scaRNA",
-            "scRNA",
-            "snoRNA",
-            "snRNA",
-            "sRNA"
-        )
-    ) {
-        "small"
-    } else if (
-        x[["biotype"]] %in% c(
-            "non_stop_decay",
-            "nonsense_mediated_decay"
-        )
-    ) {
-        "decaying"
-    } else if (
-        isTRUE(grepl(
-            pattern = "^ig_",
-            x = x[["biotype"]],
-            ignore.case = TRUE
-        ))
-    ) {
-        ## immunoglobulin
-        "ig"
-    } else if (
-        isTRUE(grepl(
-            pattern = "^tr_",
-            x = x[["biotype"]],
-            ignore.case = TRUE
-        ))
-    ) {
-        ## T cell receptor
-        "tcr"
-    } else {
-        "other"
-    }
-}
-
-
-
-#' Broad class definitions factor return
-#'
-#' @note Updated 2021-01-14.
-#' @noRd
-#'
-#' @inheritParams AcidRoxygen::params
-#' @param object `GRanges`.
-#'
-#' @return `factor`.
-.broadClass <- function(object) {
-    assert(is(object, "GRanges"))
-    colnames(mcols(object)) <- camelCase(colnames(mcols(object)), strict = TRUE)
-    ## Early return if already defined in `mcols()`.
-    if ("broadClass" %in% colnames(mcols(object))) {
-        out <- mcols(object)[["broadClass"]]
-        out <- as.factor(out)
-        return(out)
-    }
-    ## Need to strip the names on the object here, otherwise data.frame coercion
-    ## will error if the object contains duplicate names, which can happen with
-    ## GRanges that need to be split to GRangesList.
-    names(object) <- NULL
-    ## This step coerces the genomic ranges, including associated metadata
-    ## stored in `mcols()` to a flat data frame, which will now contain
-    ## seqnames, start, end, width, and strand as columns.
-    data <- as.data.frame(object)
-    ## Biotypes ----------------------------------------------------------------
-    ## Prioritizing transcript biotype over gene, if defined. This only applies
-    ## for transcript-level GRanges. For gene-level GRanges, the gene biotypes
-    ## will be used, as expected.
-    if ("txBiotype" %in% colnames(data)) {
-        biotypeCol <- "txBiotype"
-        biotypeData <- data[[biotypeCol]]
-    } else if ("geneBiotype" %in% colnames(data)) {
-        biotypeCol <- "geneBiotype"
-        biotypeData <- data[[biotypeCol]]
-    } else {
-        ## FlyBase GTF will hit this step.
-        ## Note that we're early returning without calculations in this case.
-        alertWarning(paste(
-            "{.var GRanges} does not contain biotype in {.var mcols}.",
-            "Returning without broad class definitions."
-        ))
-        ## Early `NA` return works successfully in `mcols()` construction.
-        return(NA_character_)
-    }
-    ## Gene names --------------------------------------------------------------
-    if ("geneName" %in% colnames(data)) {
-        geneNameCol <- "geneName"
-        geneNameData <- data[[geneNameCol]]
-    } else {
-        alertWarning(
-            "{.var GRanges} does not contain gene names in {.fun mcols}."
-        )
-        geneNameCol <- NULL
-        geneNameData <- NA_character_
-    }
-    ## Seqnames ----------------------------------------------------------------
-    ## This refers to the chromosome name.
-    ## Note that data frame coercion will define `seqnames` column from the
-    ## `GRanges` object (see above).
-    if ("seqnames" %in% colnames(data)) {
-        seqnamesCol <- "seqnames"
-        seqnamesData <- data[[seqnamesCol]]
-    } else {
-        ## Don't think this is currently possible to hit, but keep just in case.
-        alertWarning("{.var GRanges} does not contain {.fun seqnames}.")
-        seqnamesCol <- NULL
-        seqnamesData <- NA_character_
-    }
-    ## Apply broad class -------------------------------------------------------
-    alert(sprintf(
-        "Defining {.var broadClass} using: %s.",
-        ## Note that `c()` call here effectively removes `NULL` definitions.
-        toInlineString(sort(c(biotypeCol, geneNameCol, seqnamesCol)))
-    ))
-    ## Note that this method doesn't seem to work right with DataFrame class.
-    data <- data.frame(
-        "biotype" = biotypeData,
-        "chromosome" = seqnamesData,
-        "geneName" = geneNameData,
-        stringsAsFactors = TRUE
-    )
-    ## Consider adding BiocParallel support here for improved speed.
-    out <- apply(X = data, MARGIN = 1L, FUN = .applyBroadClass)
-    out <- as.factor(out)
-    out
-}
-
-
-
+## Detection ===================================================================
 #' Detect the GFF source information
 #'
 #' @details
@@ -416,6 +142,295 @@
 
 
 
+## Metadata ====================================================================
+#' Apply broad class definitions
+#'
+#' This function is intended to work rowwise on the GRanges mcols.
+#'
+#' @section Mitochondrial genes:
+#' Mitochondrial gene matching depends on the genome.
+#' This is important in particular for single-cell RNA-seq.
+#'
+#' - *H. sapiens*: "MT-" gene name.
+#' - *M. musculus*: "mt-" gene name.
+#' - *D. melanogaster*: "mt:" gene name.
+#' - *C. elegans*: Can't match by gene name. Match by "MtDNA" chromosome.
+#'   Alternatively, can match using "MTCE" sequence name (parent clone).
+#'   https://www.wormbase.org/species/c_elegans/clone/MTCE
+#'
+#' Note that this might not be perfect for other genomes, so consider
+#' atttempting to improve support here in a future update.
+#'
+#' @seealso Can use `dplyr::case_when()` instead, which allows for a rowwise
+#'   vectorized if/else call stack.
+#'
+#' @note Can return `NA_character_` here instead. Keeping this as "other", to
+#'   main consistency with previous data sets. Also note that `NA` can behave
+#'   inconsistently in plotting engines.
+#' @note Updated 2020-01-20.
+#'
+#' @author Rory Kirchner, Michael Steinbaugh
+#' @noRd
+#'
+#' @param x `list`.
+#'   List returned via apply call using `MARGIN = 1`.
+#'
+#' @return `character(1)`.
+.applyBroadClass <- function(x) {
+    if (
+        isTRUE(grepl(
+            pattern = "^MT",
+            x = x[["chromosome"]],
+            ignore.case = TRUE
+        )) ||
+        isTRUE(grepl(
+            pattern = "^mt[\\:\\-]",
+            x = x[["geneName"]],
+            ignore.case = TRUE
+        ))
+    ) {
+        "mito"
+    } else if (x[["biotype"]] == "protein_coding") {
+        "coding"
+    } else if (
+        x[["biotype"]] %in% c(
+            "known_ncrna",
+            "lincRNA",
+            "non_coding"
+        )
+    ) {
+        "noncoding"
+    } else if (
+        isTRUE(grepl(
+            pattern = "pseudo",
+            x = x[["biotype"]],
+            ignore.case = TRUE
+        ))
+    ) {
+        "pseudo"
+    } else if (
+        x[["biotype"]] %in% c(
+            "miRNA",
+            "misc_RNA",
+            "ribozyme",
+            "rRNA",
+            "scaRNA",
+            "scRNA",
+            "snoRNA",
+            "snRNA",
+            "sRNA"
+        )
+    ) {
+        "small"
+    } else if (
+        x[["biotype"]] %in% c(
+            "non_stop_decay",
+            "nonsense_mediated_decay"
+        )
+    ) {
+        "decaying"
+    } else if (
+        isTRUE(grepl(
+            pattern = "^ig_",
+            x = x[["biotype"]],
+            ignore.case = TRUE
+        ))
+    ) {
+        ## immunoglobulin
+        "ig"
+    } else if (
+        isTRUE(grepl(
+            pattern = "^tr_",
+            x = x[["biotype"]],
+            ignore.case = TRUE
+        ))
+    ) {
+        ## T cell receptor
+        "tcr"
+    } else {
+        "other"
+    }
+}
+
+
+
+#' Add broad class annotations
+#'
+#' @note Updated 2021-01-18.
+#' @noRd
+.addBroadClass <- function(object) {
+    assert(
+        is(object, "GRanges"),
+        hasNoDuplicates(names(object)),
+        identical(
+            x = colnames(mcols(object)),
+            y = camelCase(colnames(mcols(object)), strict = TRUE)
+        ),
+        !isSubset("broadClass", colnames(mcols(object)))
+    )
+    df <- as.data.frame(object)
+    colnames(df) <- gsub(
+        pattern = "^transcript",
+        replacement = "tx",
+        x = colnames(df)
+    )
+    ## Biotypes ----------------------------------------------------------------
+    ## Prioritizing transcript biotype over gene, if defined. This only applies
+    ## for transcript-level GRanges. For gene-level GRanges, the gene biotypes
+    ## will be used, as expected.
+    if ("txBiotype" %in% colnames(df)) {
+        biotypeCol <- "txBiotype"
+        biotypeData <- df[[biotypeCol]]
+    } else if ("geneBiotype" %in% colnames(df)) {
+        biotypeCol <- "geneBiotype"
+        biotypeData <- df[[biotypeCol]]
+    } else {
+        ## FlyBase GTF will hit this step.
+        ## Note that we're early returning without calculations in this case.
+        alertWarning(paste(
+            "{.var GRanges} does not contain biotype in {.var mcols}.",
+            "Returning without broad class definitions."
+        ))
+        ## Early `NA` return works successfully in `mcols()` construction.
+        return(NA_character_)
+    }
+    ## Gene names --------------------------------------------------------------
+    if ("geneName" %in% colnames(df)) {
+        geneNameCol <- "geneName"
+        geneNameData <- df[[geneNameCol]]
+    } else {
+        alertWarning(
+            "{.var GRanges} does not contain gene names in {.fun mcols}."
+        )
+        geneNameCol <- NULL
+        geneNameData <- NA_character_
+    }
+    ## Seqnames ----------------------------------------------------------------
+    ## This refers to the chromosome name.
+    ## Note that data frame coercion will define `seqnames` column from the
+    ## `GRanges` object (see above).
+    if ("seqnames" %in% colnames(df)) {
+        seqnamesCol <- "seqnames"
+        seqnamesData <- df[[seqnamesCol]]
+    } else {
+        ## Don't think this is currently possible to hit, but keep just in case.
+        alertWarning("{.var GRanges} does not contain {.fun seqnames}.")
+        seqnamesCol <- NULL
+        seqnamesData <- NA_character_
+    }
+    ## Apply broad class -------------------------------------------------------
+    alert(sprintf(
+        "Defining {.var broadClass} using: %s.",
+        ## Note that `c()` call here effectively removes `NULL` definitions.
+        toInlineString(sort(c(biotypeCol, geneNameCol, seqnamesCol)))
+    ))
+    ## Note that this method doesn't seem to work right with DataFrame class.
+    df <- data.frame(
+        "biotype" = biotypeData,
+        "chromosome" = seqnamesData,
+        "geneName" = geneNameData,
+        stringsAsFactors = TRUE
+    )
+    ## Consider adding BiocParallel support here for improved speed.
+    mcols(object)[["broadClass"]] <- Rle(as.factor(apply(
+        X = df,
+        MARGIN = 1L,
+        FUN = .applyBroadClass
+        )))
+    validObject(object)
+    object
+}
+
+
+
+#' Add Ensembl gene synonyms
+#'
+#' @note Updated 2021-01-18.
+#' @noRd
+.addGeneSynonyms <- function(object) {
+    assert(
+        is(object, "GRanges"),
+        hasNoDuplicates(names(object)),
+        identical(
+            x = colnames(mcols(object)),
+            y = camelCase(colnames(mcols(object)), strict = TRUE)
+        ),
+        !isSubset("geneSynonyms", colnames(mcols(object))),
+        isSubset("geneId", colnames(mcols))
+    )
+    mcols <- mcols(object)
+    if (!any(grepl(pattern = "^ENS", x = mcols[["geneId"]]))) {
+        return(object)
+    }
+    organism <- organism(object)
+    if (!isSubset(organism, eval(formals(geneSynonyms)[["organism"]]))) {
+        return(object)
+    }
+    alert(sprintf(
+        "Adding gene synonyms to {.var %s} column.",
+        "geneSynonyms"
+    ))
+    synonyms <- geneSynonyms(organism = organism, return = "DataFrame")
+    assert(identical(c("geneId", "geneSynonyms"), colnames(synonyms)))
+    mcols <- leftJoin(x = mcols, y = synonyms, by = "geneId")
+    mcols(object) <- mcols
+    object
+}
+
+
+
+## FIXME NEED TO ADD `.addGeneVersion` function here.
+
+
+
+#' Add the transcript version
+#'
+#' Append the transcript version to the identifier (e.g. ENST00000000233.10).
+#'
+#' @note Updated 2021-01-18.
+#' @noRd
+.addTxVersion <- function(object) {
+    assert(
+        is(object, "GRanges"),
+        identical(metadata(object)[["level"]], "transcripts"),
+        isSubset("txId", colnames(mcols(object)))
+    )
+    alert("Adding version to transcript identifiers.")
+    mcolnames <- colnames(mcols(object))
+    if (isSubset("txIdVersion", mcolnames)) {
+        ## `makeGRangesFromEnsembl()` output via ensembldb.
+        id <- mcols(object)[["txIdVersion"]]
+    } else if (isSubset("txVersion", mcolnames)) {
+        ## `makeGRangesFromGFF()` output.
+        id <- mcols(object)[["txId"]]
+        version <- mcols(object)[["txVersion"]]
+        id <- Rle(paste(id, version, sep = "."))
+    } else {
+        stop("Failed to locate transcript version metadata.")  # nolint
+    }
+    mcols(object)[["txId"]] <- id
+    ## Note that names are set by `.makeGRanges()`.
+    object
+}
+
+
+
+## Source-specific parsers =====================================================
+## FIXME ADD makeGRangesFromWormBase
+## FIXME ADD makeGRangesFromFlyBase
+## FIXME ADD makeGRangesFromRefSeq
+##
+## FIXME NEEDS TO REIMPORT GTF/GFF INTO GRANGES AND WORK FROM THERE.
+## FIXME (each of these need to support level argument)
+
+## Legacy code:
+## https://github.com/acidgenomics/r-acidgenomes/blob/main/R/makeGRangesFromGFF-FlyBase.R
+## https://github.com/acidgenomics/r-acidgenomes/blob/main/R/makeGRangesFromGFF-RefSeq.R
+## https://github.com/acidgenomics/r-acidgenomes/blob/main/R/makeGRangesFromGFF-WormBase.R
+
+
+
+## Main generator ==============================================================
 ## FIXME ignoreVersion = FALSE needs to return gene ID versions.
 ## FIXME NEED TO IMPROVE CONSISTENCY Of METADATA RETURN.
 ## FIXME NEED TO DETECT ORGANISM AUTOMATICALLY.
@@ -529,20 +544,6 @@
     validObject(object)
     object
 }
-
-
-
-## FIXME ADD makeGRangesFromWormBase
-## FIXME ADD makeGRangesFromFlyBase
-## FIXME ADD makeGRangesFromRefSeq
-##
-## FIXME NEEDS TO REIMPORT GTF/GFF INTO GRANGES AND WORK FROM THERE.
-## FIXME (each of these need to support level argument)
-
-## Legacy code:
-## https://github.com/acidgenomics/r-acidgenomes/blob/main/R/makeGRangesFromGFF-FlyBase.R
-## https://github.com/acidgenomics/r-acidgenomes/blob/main/R/makeGRangesFromGFF-RefSeq.R
-## https://github.com/acidgenomics/r-acidgenomes/blob/main/R/makeGRangesFromGFF-WormBase.R
 
 
 
