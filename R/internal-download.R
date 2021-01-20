1e768d4994a9d7c579294a935ba42eedcbba764b
Michael Steinbaugh <mike@steinbaugh.com>
2021-01-20 12:05:09 -0500

    Migrate internal code

---
+++ R/internal-download.R
@@ -0,0 +1,32 @@
+#' Download multiple genome files in a single call
+#'
+#' @note Updated 2021-01-07.
+#' @noRd
+#'
+#' @return `character`
+#'   Destination files.
+.downloadURLs <- function(urls, outputDir) {
+    assert(
+        allAreURLs(urls),
+        isString(outputDir)
+    )
+    outputDir <- initDir(outputDir)
+    destfiles <- vapply(
+        X = urls,
+        FUN = function(url) {
+            file.path(outputDir, basename(url))
+        },
+        FUN.VALUE = character(1L)
+    )
+    assert(identical(names(urls), names(destfiles)))
+    out <- mapply(
+        url = urls,
+        destfile = destfiles,
+        FUN = download,
+        SIMPLIFY = TRUE,
+        USE.NAMES = FALSE
+    )
+    names(out) <- names(urls)
+    assert(allAreFiles(out))
+    invisible(out)
+}
