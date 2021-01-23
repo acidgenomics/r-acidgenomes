ec25187ebe4b09233127c47931dcba3d323b799c
Michael Steinbaugh <mike@steinbaugh.com>
2021-01-23 08:53:36 -0500

    Harden function and ensure we return useful metadata

--- R/makeEnsDbFromGFF.R
+++ R/makeEnsDbFromGFF.R
@@ -1,3 +1,9 @@
+## NOTE Need to resolve/suppress this warning:
+## Warning in for (i in (1L:cols)[do]) { :
+##   closing unused connection 3 (ftp://ftp.ensembl.org/pub/release-102/mysql/)
+
+
+
 #' Make EnsDb object from a GFF/GTF file
 #'
 #' Wrapper for ensembldb importer functions.
@@ -15,37 +21,38 @@
 #' - `ensembldb::ensDbFromGtf()`.
 #'
 #' @examples
-#' file <- pasteURL(AcidGenomesTestsURL, "ensembl.gtf")
+#' file <- pasteURL(
+#'     "ftp.ensembl.org",
+#'     "pub",
+#'     "release-102",
+#'     "gtf",
+#'     "homo_sapiens",
+#'     "Homo_sapiens.GRCh38.102.gtf.gz",
+#'     protocol = "ftp"
+#' )
 #' edb <- makeEnsDbFromGFF(file)
+#' print(edb)
 makeEnsDbFromGFF <- function(file) {
     assert(isString(file))
-    requireNamespaces("ensembldb")
-    alert(sprintf("Making {.var %s} from {.file %s}.", "EnsDb", file))
-    args <- list()
-    meta <- getGFFMetadata(file)
-    args[["outfile"]] <- tempfile()
-    args[["genomeBuild"]] <- meta[["genomeBuild"]]
-    args[["organism"]] <- meta[["organism"]]
-    args[["version"]] <- meta[["release"]]
-    pattern <- .gffPatterns[["ensembl"]]
-    if (isTRUE(grepl(pattern = pattern, x = basename(file)))) {
-        x <- str_match(
-            string = basename(file),
-            pattern = pattern
-        )[1L, , drop = TRUE]
-        if (!isString(args[["organism"]])) {
-            args[["organism"]] <-
-                gsub(pattern = "_", replacement = " ", x = x[[3L]])
-        }
-        if (!isString(args[["genomeBuild"]])) {
-            args[["genomeBuild"]] <- x[[4L]]
-        }
-        if (!isInt(args[["version"]])) {
-            args[["version"]] <- as.integer(x[[5L]])
-        }
+    if (isAFile(file)) {
+        file <- realpath(file)
     }
+    pattern <- .gffPatterns[["ensembl"]]
+    assert(isMatchingRegex(pattern = pattern, x = basename(file)))
+    alert(sprintf("Making {.var %s} from {.file %s}.", "EnsDb", file))
+    requireNamespaces("ensembldb")
+    args <- list()
+    x <- str_match(
+        string = basename(file),
+        pattern = pattern
+    )[1L, , drop = TRUE]
+    args[["genomeBuild"]] <- x[[4L]]
+    args[["organism"]] <- gsub(pattern = "_", replacement = " ", x = x[[3L]])
+    args[["version"]] <- as.integer(x[[5L]])
+    args[["outfile"]] <- tempfile()
     ext <- fileExt(file)
     tmpfile <- .cacheIt(file)
+    meta <- getGFFMetadata(file)
     if (grepl(pattern = "gff", x = ext, ignore.case = TRUE)) {
         what <- ensembldb::ensDbFromGff
         args <- append(x = args, values = list("gff" = tmpfile))
@@ -58,9 +65,10 @@ makeEnsDbFromGFF <- function(file) {
     suppressWarnings({
         suppressMessages({
             sqlite <- do.call(what = what, args = args)
+            edb <- ensembldb::EnsDb(x = sqlite)
+            file.remove(sqlite)
         })
     })
-    edb <- ensembldb::EnsDb(x = sqlite)
     assert(is(edb, "EnsDb"))
     attr(x = edb, which = "args") <- args
     attr(x = edb, which = "gffMetadata") <- meta
