f4c60bee63bfbe256bd0072fe0fb5c6cd63861c2
Michael Steinbaugh <mike@steinbaugh.com>
2021-01-23 08:54:01 -0500

    Harden function

--- R/makeGRangesFromTxDb.R
+++ R/makeGRangesFromTxDb.R
@@ -1,30 +1,6 @@
-## FIXME NEED TO HANDLE THIS
-##> genes <- genes(db)
-##2714 genes were dropped because
-##they have exons located on both
-##strands of the same reference
-##sequence or on more than one
-##reference sequence, so cannot be
-##represented by a single genomic
-##range.
-##Use
-##'single.strand.genes.only=FALSE' to
-##get all the genes in a GRangesList
-##object, or use suppressMessages()
-##to suppress this message.
-
-## transcripts don't use an identifier, which is confusing.
-## names should be defined as tx_name in this case instead.
-
-## FIXME tx_name contains duplicates, so need to figure out how to handle.
-
-
-
-## FIXME NEED TO RETHINK SUPPORT FOR REFSEQ HERE.
-
 #' Make GRanges from TxDb object
 #'
-#' @note Updated 2021-01-14.
+#' @note Updated 2021-01-22.
 #' @noRd
 #'
 #' @return `GRanges`.
@@ -35,13 +11,11 @@
     assert(is(object, "TxDb"))
     level <- match.arg(level)
     args <- list("x" = object)
-    ## FIXME THIS RETURNS INCORRECTLY FOR DROSOPHILA.
-    ## FIXME NEED TO PARSE THE GTF DIRECTLY TO GET METADATA...
     keys <- AnnotationDbi::columns(object)
     switch(
         EXPR = level,
         "genes" = {
-            fun <- GenomicFeatures::genes
+            what <- GenomicFeatures::genes
             columns <- "gene_id"
             args <- append(
                 x = args,
@@ -51,12 +25,14 @@
             )
         },
         "transcripts" = {
-            fun <- GenomicFeatures::transcripts
+            what <- GenomicFeatures::transcripts
             columns <- c("tx_id", "tx_name", "gene_id")
         }
     )
     args[["columns"]] <- columns
-    gr <- do.call(what = fun, args = args)
+    suppressMessages({
+        gr <- do.call(what = what, args = args)
+    })
     assert(is(gr, "GRanges"))
     gr
 }
