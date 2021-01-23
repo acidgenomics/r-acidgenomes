de4229e565b3633e27f9833a1c1a33e98ad6a5f5
Michael Steinbaugh <mike@steinbaugh.com>
2021-01-23 08:28:43 -0500

    Rework, don't allow the user to set organism, genomeBuild, release

--- R/makeEnsDbFromGFF.R
+++ R/makeEnsDbFromGFF.R
@@ -3,7 +3,7 @@
 #' Wrapper for ensembldb importer functions.
 #'
 #' @name makeEnsDbFromGFF
-#' @note Updated 2021-01-14.
+#' @note Updated 2021-01-23.
 #'
 #' @inheritParams AcidRoxygen::params
 #' @inheritParams params
@@ -16,62 +16,36 @@
 #'
 #' @examples
 #' file <- pasteURL(AcidGenomesTestsURL, "ensembl.gtf")
-#' edb <- makeEnsDbFromGFF(
-#'     file = file,
-#'     organism = "Homo sapiens",
-#'     genomeBuild = "GRCh38",
-#'     release = 100L
-#' )
-NULL
-
-
-
-#' @describeIn makeEnsDbFromGFF Primary function.
-#' @export
-makeEnsDbFromGFF <- function(
-    file,
-    organism = NULL,
-    genomeBuild = NULL,
-    release = NULL
-) {
-    assert(
-        isString(file),
-        isString(organism, nullOK = TRUE),
-        isString(genomeBuild, nullOK = TRUE),
-        isInt(release, nullOK = TRUE)
-    )
+#' edb <- makeEnsDbFromGFF(file)
+makeEnsDbFromGFF <- function(file) {
+    assert(isString(file))
     requireNamespaces("ensembldb")
     alert(sprintf("Making {.var %s} from {.file %s}.", "EnsDb", file))
-    tmpfile <- .cacheIt(file)
-    ext <- fileExt(file)
-    args <- list("outfile" = tempfile())
+    args <- list()
+    meta <- getGFFMetadata(file)
+    args[["outfile"]] <- tempfile()
+    args[["genomeBuild"]] <- meta[["genomeBuild"]]
+    args[["organism"]] <- meta[["organism"]]
+    args[["version"]] <- meta[["release"]]
     pattern <- .gffPatterns[["ensembl"]]
     if (isTRUE(grepl(pattern = pattern, x = basename(file)))) {
-        alert("Detecting genome metadata from file name.")
-        match <- str_match(string = basename(file), pattern = pattern)
-        organism <- gsub(pattern = "_", replacement = " ", x = match[1L, 3L])
-        genomeBuild <- match[1L, 4L]
-        release <- as.integer(match[1L, 5L])
-    } else if (any(is.null(organism), is.null(genomeBuild), is.null(release))) {
-        ## nocov start
-        stop(sprintf(
-            paste(
-                "Failed to match genome metadata from file name ('%s').",
-                "Define values manually: %s."
-            ),
-            file,
-            toString(c("organism", "genomeBuild", "release"))
-        ))
-        ## nocov end
+        x <- str_match(
+            string = basename(file),
+            pattern = pattern
+        )[1L, , drop = TRUE]
+        if (!isString(args[["organism"]])) {
+            args[["organism"]] <-
+                gsub(pattern = "_", replacement = " ", x = x[[3L]])
+        }
+        if (!isString(args[["genomeBuild"]])) {
+            args[["genomeBuild"]] <- x[[4L]]
+        }
+        if (!isInt(args[["version"]])) {
+            args[["version"]] <- as.integer(x[[5L]])
+        }
     }
-    args <- append(
-        x = args,
-        values = list(
-            "organism" = organism,
-            "genomeVersion" = genomeBuild,
-            "version" = release
-        )
-    )
+    ext <- fileExt(file)
+    tmpfile <- .cacheIt(file)
     if (grepl(pattern = "gff", x = ext, ignore.case = TRUE)) {
         what <- ensembldb::ensDbFromGff
         args <- append(x = args, values = list("gff" = tmpfile))
@@ -88,12 +62,8 @@ makeEnsDbFromGFF <- function(
     })
     edb <- ensembldb::EnsDb(x = sqlite)
     assert(is(edb, "EnsDb"))
+    attr(x = edb, which = "args") <- args
+    attr(x = edb, which = "gffMetadata") <- meta
     validObject(edb)
     edb
 }
-
-
-
-#' @describeIn makeEnsDbFromGFF Alias for GTF files.
-#' @export
-makeEnsDbFromGTF <- makeEnsDbFromGFF
