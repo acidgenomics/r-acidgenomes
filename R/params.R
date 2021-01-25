c13bf223ee341758640798ca2461ed381291d3f3
Michael Steinbaugh <mike@steinbaugh.com>
2021-01-25 13:39:37 -0500

    Don't expose these params to user

--- R/params.R
+++ R/params.R
@@ -2,9 +2,6 @@
 #' @inherit AcidRoxygen::params return title
 #' @keywords internal
 #'
-#' @param broadClass `logical(1)`.
-#'   Include broad class definitions.
-#' @param seqinfo `Seqinfo` or `NULL`.
 #' @param synonyms `logical(1)`.
 #'   Include gene synonyms.
 #'   Queries the Ensembl web server, and is CPU intensive.
