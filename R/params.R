e1822964b176498e8b9fa34810ae46dba0623a99
Michael Steinbaugh <mike@steinbaugh.com>
2021-01-25 13:02:47 -0500

    Document seqinfo pass-in

--- R/params.R
+++ R/params.R
@@ -4,6 +4,7 @@
 #'
 #' @param broadClass `logical(1)`.
 #'   Include broad class definitions.
+#' @param seqinfo `Seqinfo` or `NULL`.
 #' @param synonyms `logical(1)`.
 #'   Include gene synonyms.
 #'   Queries the Ensembl web server, and is CPU intensive.
