42909e2741b5bd6de1d5cec44aab75100a8fb7df
Michael Steinbaugh <mike@steinbaugh.com>
2021-01-25 13:03:34 -0500

    Consolidate

--- R/internal.R
+++ R/internal.R
@@ -1,3 +1,38 @@
+#' Download multiple genome files in a single call
+#'
+#' @note Updated 2021-01-07.
+#' @noRd
+#'
+#' @return `character`
+#'   Destination files.
+.downloadURLs <- function(urls, outputDir) {
+    assert(
+        allAreURLs(urls),
+        isString(outputDir)
+    )
+    outputDir <- initDir(outputDir)
+    destfiles <- vapply(
+        X = urls,
+        FUN = function(url) {
+            file.path(outputDir, basename(url))
+        },
+        FUN.VALUE = character(1L)
+    )
+    assert(identical(names(urls), names(destfiles)))
+    out <- mapply(
+        url = urls,
+        destfile = destfiles,
+        FUN = download,
+        SIMPLIFY = TRUE,
+        USE.NAMES = FALSE
+    )
+    names(out) <- names(urls)
+    assert(allAreFiles(out))
+    invisible(out)
+}
+
+
+
 #' Get an internal function from the package
 #'
 #' @note Updated 2021-01-25.
