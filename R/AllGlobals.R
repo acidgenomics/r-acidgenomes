059ed854b78c22e94ac917b60f5a35bc08e09fae
Michael Steinbaugh <mike@steinbaugh.com>
2021-01-28 09:24:31 -0500

    Improve internal blacklist checks

--- R/AllGlobals.R
+++ R/AllGlobals.R
@@ -11,10 +11,10 @@ NULL
 .gffPatterns <- list(
     "ensembl" = paste0(
         ## "Homo_sapiens.GRCh38.102.gtf.gz"
-        "^([a-z0-9]+_)?",              # BiocFileCache.
-        "([A-Z][a-z]+_[a-z]+)",        # "Homo_sapiens".
+        "^([a-z0-9]+_)?",              # BiocFileCache
+        "([A-Z][a-z]+_[a-z]+)",        # "Homo_sapiens"
         "\\.([A-Za-z0-9]+)",           # "GRCh38"
-        "\\.([0-9]+)",                 # "102".
+        "\\.([0-9]+)",                 # "102"
         "(\\.chr_patch_hapl_scaff)?",
         "\\.(gff3|gtf)",
         "(\\.gz)?$"
@@ -30,10 +30,10 @@ NULL
     ),
     "gencode" = paste0(
         ## "gencode.v36.annotation.gtf.gz"
-        "^([a-z0-9]+_)?",  # BiocFileCache.
+        "^([a-z0-9]+_)?",  # BiocFileCache
         "gencode",
         "\\.v([M0-9]+)",   # "36" (human) / "M25" (mouse)
-        "(lift37)?",       # GRCh37-specific.
+        "(lift37)?",       # GRCh37-specific
         "\\.annotation",
         "\\.(gff3|gtf)",
         "(\\.gz)?$"
@@ -41,28 +41,28 @@ NULL
     "refseq" = paste0(
         ## "GCF_000001405.38_GRCh38.p12_genomic.gff.gz"
         ## "GCA_000001405.15_GRCh38_full_analysis_set.refseq_annotation.gff.gz"
-        "^([0-9a-z]_)?",             # BiocFileCache.
-        "(GC[AF]_[0-9]+\\.[0-9]+)",  # "GCF_000001405.38.
-        "_([^_]+)",                  # "GRCh38.p12".
-        "_(.+)",                     # "genomic" or "full_analysis_set".
+        "^([0-9a-z]_)?",             # BiocFileCache
+        "(GC[AF]_[0-9]+\\.[0-9]+)",  # "GCF_000001405.38"
+        "_([^_]+)",                  # "GRCh38.p12"
+        "_(.+)",                     # "genomic" or "full_analysis_set"
         "\\.(gff|gtf)",
         "(\\.gz)?$"
     ),
     "ucsc" = paste0(
         ## "hg38.ensGene.gtf.gz"
-        "^([0-9a-z]_)?",                              # BiocFileCache.
-        "([a-z]+[A-Za-z]+[0-9]+)",                    # "hg38".
+        "^([0-9a-z]_)?",                              # BiocFileCache
+        "([a-z]+[A-Za-z]+[0-9]+)",                    # "hg38"
         "\\.(ensGene|knownGene|ncbiRefSeq|refGene)",  # "ensGene"
         "\\.gtf",
         "(\\.gz)?$"
     ),
     "wormbase" = paste0(
         ## "c_elegans.PRJNA13758.WS279.canonical_geneset.gtf.gz"
-        "^([a-z0-9]+_)?",   # BiocFileCache.
-        "^([a-z]_[a-z]+)",  # "c_elegans".
-        "\\.([A-Z0-9]+)",   # "PRJNA13758".
-        "\\.(WS[0-9]+)",    # "WS279".
-        "\\.([a-z_]+)",     # "canonical_geneset".
+        "^([a-z0-9]+_)?",   # BiocFileCache
+        "^([a-z]_[a-z]+)",  # "c_elegans"
+        "\\.([A-Z0-9]+)",   # "PRJNA13758"
+        "\\.(WS[0-9]+)",    # "WS279"
+        "\\.([a-z_]+)",     # "canonical_geneset"
         "\\.(gff3|gtf)",
         "(\\.gz)?$"
     )
