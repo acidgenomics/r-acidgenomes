00284a1bc85aeac2d75ac1b5369ba4a25685ed3c
Michael Steinbaugh <mike@steinbaugh.com>
2021-02-01 09:12:41 -0500

    Improve code coverage

--- tests/testthat/test-makeGRangesFromEnsembl.R
+++ tests/testthat/test-makeGRangesFromEnsembl.R
@@ -8,29 +8,38 @@ test_that("Genes", {
     object <- makeGRangesFromEnsembl(
         organism = organism,
         level = "genes",
-        release = release
+        release = ensemblRelease,
+        ignoreVersion = TRUE
     )
-    expect_s4_class(object, "GRanges")
+    expect_s4_class(object, "EnsemblGenes")
     expect_identical(
-        object = metadata(object)[
-            c("ensemblRelease", "genomeBuild", "id", "level", "organism")],
+        object = metadata(object)[c(
+            "annotationHubId",
+            "genomeBuild",
+            "organism",
+            "release"
+        )],
         expected = list(
-            ensemblRelease = 97L,
-            genomeBuild = "GRCh38",
-            id = "AH73881",
-            level = "genes",
-            organism = "Homo sapiens"
+            "annotationHubId" = "AH89180",
+            "genomeBuild" = "GRCh38",
+            "organism" = "Homo sapiens",
+            "release" = 102L
         )
     )
-    expect_identical(length(object), 67667L)
+    expect_identical(length(object), 68001L)
     expect_identical(
         object = head(names(object), 3L),
-        expected = c("ENSG00000000003", "ENSG00000000005", "ENSG00000000419")
+        expected = c(
+            "ENSG00000228572",
+            "ENSG00000182378",
+            "ENSG00000226179"
+        )
     )
     expect_identical(
         object = lapply(mcols(object), class),
         expected = list(
             "broadClass" = Rle,
+            "canonicalTranscript" = Rle,
             "description" = Rle,
             "entrezId" = "list",
             "geneBiotype" = Rle,
@@ -47,30 +56,60 @@ test_that("Transcripts", {
     object <- makeGRangesFromEnsembl(
         organism = organism,
         level = "transcripts",
-        release = release,
+        release = ensemblRelease,
         ignoreVersion = FALSE
     )
-    expect_s4_class(object, "GRanges")
-    expect_identical(length(object), 248916L)
+    expect_s4_class(object, "EnsemblTranscripts")
+    expect_identical(length(object), 254853L)
     expect_identical(
         object = head(names(object), n = 2L),
-        expected = c("ENST00000000233.10", "ENST00000000412.8")
+        expected = c("ENST00000635602.1", "ENST00000635506.1")
+    )
+    expect_identical(
+        object = as.character(head(mcols(object)[["geneId"]], n = 2L)),
+        expected = c("ENSG00000283061.1", "ENSG00000283061.1")
+    )
+    expect_identical(
+        object = as.character(head(mcols(object)[["geneIdNoVersion"]], n = 2L)),
+        expected = c("ENSG00000283061", "ENSG00000283061")
+    )
+    expect_identical(
+        object = as.character(head(mcols(object)[["geneIdVersion"]], n = 2L)),
+        expected = c("ENSG00000283061.1", "ENSG00000283061.1")
+    )
+    expect_identical(
+        object = as.character(head(mcols(object)[["txId"]], n = 2L)),
+        expected = c("ENST00000635602.1", "ENST00000635506.1")
+    )
+    expect_identical(
+        object = as.character(head(mcols(object)[["txIdNoVersion"]], n = 2L)),
+        expected = c("ENST00000635602", "ENST00000635506")
+    )
+    expect_identical(
+        object = as.character(head(mcols(object)[["txIdVersion"]], n = 2L)),
+        expected = c("ENST00000635602.1", "ENST00000635506.1")
     )
     expect_identical(
         object = lapply(mcols(object), class),
         expected = list(
             "broadClass" = Rle,
+            "canonicalTranscript" = Rle,
             "description" = Rle,
             "entrezId" = "list",
+            "gcContent" = Rle,
             "geneBiotype" = Rle,
             "geneId" = Rle,
+            "geneIdNoVersion" = Rle,
             "geneIdVersion" = Rle,
-            "geneName = Rle,
+            "geneName" = Rle,
+            "geneSeqEnd" = Rle,
+            "geneSeqStart" = Rle,
             "seqCoordSystem" = Rle,
             "txBiotype" = Rle,
             "txCdsSeqEnd" = Rle,
             "txCdsSeqStart" = Rle,
             "txId" = Rle,
+            "txIdNoVersion" = Rle,
             "txIdVersion" = Rle,
             "txName" = Rle,
             "txSupportLevel" = Rle
