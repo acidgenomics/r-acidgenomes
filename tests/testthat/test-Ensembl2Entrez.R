1f24b078a5851ae95cb28ba6db48bf95ff61cd19
Michael Steinbaugh <mike@steinbaugh.com>
2021-02-01 10:07:51 -0500

    Update code coverage

--- tests/testthat/test-Ensembl2Entrez.R
+++ tests/testthat/test-Ensembl2Entrez.R
@@ -4,15 +4,17 @@ formats <- eval(formals(`Ensembl2Entrez,GRanges`)[["format"]])
 
 test_that("character", {
     genes <- c(
-        "ENSG00000000003", "ENSG00000000005",
-        "ENSG00000004866", "ENSG00000063587"
+        "ENSG00000000003",
+        "ENSG00000000005",
+        "ENSG00000004866",
+        "ENSG00000063587"
     )
     ## 1:1 mapping.
     object <- Ensembl2Entrez(genes, format = "1:1")
     expect_identical(metadata(object)[["format"]], "1:1")
     expected <- DataFrame(
-        ensembl = genes,
-        entrez = c(7105L, 64102L, 7982L, 10838L),
+        "ensemblId" = genes,
+        "entrezId" = c(7105L, 64102L, 7982L, 10838L),
         row.names = genes
     )
     expect_identical(
@@ -23,17 +25,21 @@ test_that("character", {
     object <- Ensembl2Entrez(genes, format = "long")
     expect_identical(metadata(object)[["format"]], "long")
     expected <- DataFrame(
-        ensembl = c(
+        "ensemblId" = c(
             "ENSG00000000003",
             "ENSG00000000005",
-            "ENSG00000004866", "ENSG00000004866",
-            "ENSG00000063587", "ENSG00000063587"
+            "ENSG00000004866",
+            "ENSG00000004866",
+            "ENSG00000063587",
+            "ENSG00000063587"
         ),
-        entrez = c(
+        "entrezId" = c(
             7105L,
             64102L,
-            7982L, 93655L,
-            10838L, 105373378L
+            7982L,
+            93655L,
+            10838L,
+            105373378L
         )
     )
     expect_identical(
@@ -62,16 +68,17 @@ context("Entrez2Ensembl")
 
 test_that("character", {
     ## These are from the Ensembl return above.
-    genes <- c(
-        7105L, 7982L, 10838L,
-        64102L, 93655L, 105373378L
-    )
+    genes <- c(7105L, 7982L, 10838L, 64102L, 93655L, 105373378L)
     ## 1:1 mapping (of input keys, note the expected Ensembl dupes here).
-    object <- Entrez2Ensembl(genes, format = "1:1")
+    object <- Entrez2Ensembl(
+        object = genes,
+        organism = "Homo sapiens",
+        format = "1:1"
+    )
     expect_identical(metadata(object)[["format"]], "1:1")
     expected <- DataFrame(
-        entrez = genes,
-        ensembl = c(
+        "entrezId" = genes,
+        "ensemblId" = c(
             "ENSG00000000003",
             "ENSG00000004866",
             "ENSG00000063587",
@@ -86,11 +93,15 @@ test_that("character", {
         expected = as.data.frame(expected)
     )
     ## Long format (non-unique).
-    object <- Entrez2Ensembl(genes, format = "long")
+    object <- Entrez2Ensembl(
+        object = genes,
+        organism = "Homo sapiens",
+        format = "long"
+    )
     expect_identical(metadata(object)[["format"]], "long")
     expected <- DataFrame(
-        entrez = genes,
-        ensembl = c(
+        "entrezId" = genes,
+        "ensemblId" = c(
             "ENSG00000000003",
             "ENSG00000004866",
             "ENSG00000063587",
