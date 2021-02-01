e85a618c141b7909fe79c13635774ce7cdc9b4cd
Michael Steinbaugh <mike@steinbaugh.com>
2021-02-01 15:28:12 -0500

    Update test

--- tests/testthat/test-mapEnsemblReleaseToURL.R
+++ tests/testthat/test-mapEnsemblReleaseToURL.R
@@ -2,9 +2,9 @@ context("mapEnsemblReleaseToURL")
 
 skip_if_not(hasInternet(url = "https://ensembl.org/"))
 
-test_that("Ensembl 98", {
+test_that("Ensembl 100", {
     expect_identical(
-        object = mapEnsemblReleaseToURL(98L),
-        expected = "http://sep2019.archive.ensembl.org"
+        object = mapEnsemblReleaseToURL(100L),
+        expected = "https://apr2020.archive.ensembl.org"
     )
 })
