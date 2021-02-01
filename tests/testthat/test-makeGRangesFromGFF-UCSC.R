2580513f2cda63f842151b159401db7bf70bebf9
Michael Steinbaugh <mike@steinbaugh.com>
2021-02-01 15:20:51 -0500

    Add code coverage

--- tests/testthat/test-makeGRangesFromGFF-UCSC.R
+++ tests/testthat/test-makeGRangesFromGFF-UCSC.R
@@ -3,12 +3,48 @@ context("makeGRangesFromGFF : UCSC")
 skip_if_not(hasInternet())
 
 Rle <- structure("Rle", package = "S4Vectors")  # nolint
-file <- file.path("cache", "ucsc.gtf")
+file <- gffs[["ucsc_hg38_ensgene_gtf"]]
 
 test_that("GTF genes", {
-    stop("FIXME")
+    object <- makeGRangesFromGFF(
+        file = file,
+        level = "genes",
+        ignoreVersion = TRUE
+    )
+    expect_s4_class(object, "UCSCGenes")
+    expect_identical(length(object), 64252L)
+    expect_identical(names(object)[[1L]], "ENSG00000223972")
+    expect_identical(
+        object = lapply(mcols(object), class),
+        expected = list(
+            "geneId" = Rle,
+            "geneName" = Rle
+        )
+    )
 })
 
 test_that("GTF transcripts", {
-    stop("FIXME")
+    object <- makeGRangesFromGFF(
+        file = file,
+        level = "transcripts",
+        ignoreVersion = FALSE
+    )
+    expect_s4_class(object, "UCSCTranscripts")
+    expect_identical(length(object), 208239L)
+    expect_identical(names(object)[[1L]], "ENST00000456328")
+    expect_identical(
+        object = lapply(mcols(object), class),
+        expected = list(
+            "geneId" = Rle,
+            "geneName" = Rle,
+            "txBiotype" = Rle,
+            "txChrom" = Rle,
+            "txEnd" = Rle,
+            "txId" = Rle,
+            "txName" = Rle,
+            "txNumber" = Rle,
+            "txStart" = Rle,
+            "txStrand" = Rle
+        )
+    )
 })
