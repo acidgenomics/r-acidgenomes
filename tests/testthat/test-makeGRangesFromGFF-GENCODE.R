50506444e7fdba1d676a80350f000dbc367766f8
Michael Steinbaugh <mike@steinbaugh.com>
2021-02-01 11:27:58 -0500

    Update unit tests

--- tests/testthat/test-makeGRangesFromGFF-GENCODE.R
+++ tests/testthat/test-makeGRangesFromGFF-GENCODE.R
@@ -6,16 +6,22 @@ Rle <- structure("Rle", package = "S4Vectors")  # nolint
 file <- file.path("cache", "gencode.gtf")
 
 test_that("GTF genes", {
-    object <- makeGRangesFromGFF(file = file, level = "genes")
-    expect_s4_class(object, "GRanges")
+    object <- makeGRangesFromGFF(
+        file = file,
+        level = "genes",
+        ignoreVersion = TRUE
+    )
+    expect_s4_class(object, "GencodeGenes")
     expect_identical(length(object), 60L)
-    expect_identical(names(object)[[1L]], "ENSG00000177757.2")
+    expect_identical(names(object)[[1L]], "ENSG00000223972.5")
     expect_identical(
         object = lapply(mcols(object), class),
         expected = list(
             "broadClass" = Rle,
             "geneBiotype" = Rle,
             "geneId" = Rle,
+            "geneIdNoVersion" = Rle,
+            "geneIdVersion" = Rle,
             "geneName" = Rle,
             "havanaGene" = Rle,
             "level" = Rle,
@@ -27,10 +33,14 @@ test_that("GTF genes", {
 })
 
 test_that("GTF transcripts", {
-    object <- makeGRangesFromGFF(file = file, level = "transcripts")
-    expect_s4_class(object, "GRanges")
+    object <- makeGRangesFromGFF(
+        file = file,
+        level = "transcripts",
+        ignoreVersion = FALSE
+    )
+    expect_s4_class(object, "GencodeTranscripts")
     expect_identical(length(object), 167L)
-    expect_identical(names(object)[[1L]], "ENST00000326734.2")
+    expect_identical(names(object)[[1L]], "ENST00000456328.2")
     expect_identical(
         object = lapply(mcols(object), class),
         expected = list(
@@ -38,6 +48,8 @@ test_that("GTF transcripts", {
             "ccdsId" = Rle,
             "geneBiotype" = Rle,
             "geneId" = Rle,
+            "geneIdNoVersion" = Rle,
+            "geneIdVersion" = Rle,
             "geneName" = Rle,
             "havanaGene" = Rle,
             "havanaTranscript" = Rle,
@@ -48,6 +60,8 @@ test_that("GTF transcripts", {
             "tag" = Rle,
             "txBiotype" = Rle,
             "txId" = Rle,
+            "txIdNoVersion" = Rle,
+            "txIdVersion" = Rle,
             "txName" = Rle,
             "txSupportLevel" = Rle,
             "type" = Rle
@@ -58,30 +72,42 @@ test_that("GTF transcripts", {
 file <- file.path("cache", "gencode.gff3")
 
 test_that("GFF3 genes", {
-    object <- makeGRangesFromGFF(file = file, level = "genes")
-    expect_s4_class(object, "GRanges")
+    object <- makeGRangesFromGFF(
+        file = file,
+        level = "genes",
+        ignoreVersion = FALSE
+    )
+    expect_s4_class(object, "GencodeGenes")
     expect_identical(length(object), 60L)
-    expect_identical(names(object)[[1L]], "ENSG00000177757.2")
+    expect_identical(names(object)[[1L]], "ENSG00000223972.5")
     expect_identical(
         object = lapply(mcols(object), class),
         expected = list(
             "broadClass" = Rle,
             "geneBiotype" = Rle,
             "geneId" = Rle,
+            "geneIdNoVersion" = Rle,
+            "geneIdVersion" = Rle,
             "geneName" = Rle,
             "havanaGene" = Rle,
             "level" = Rle,
+            "ont" = "CompressedCharacterList",
             "source" = Rle,
+            "tag" = "CompressedCharacterList",
             "type" = Rle
         )
     )
 })
 
 test_that("GFF3 transcripts", {
-    object <- makeGRangesFromGFF(file = file, level = "transcripts")
-    expect_s4_class(object, "GRanges")
+    object <- makeGRangesFromGFF(
+        file = file,
+        level = "transcripts",
+        ignoreVersion = FALSE
+    )
+    expect_s4_class(object, "GencodeTranscripts")
     expect_identical(length(object), 167L)
-    expect_identical(names(object)[[1L]], "ENST00000326734.2")
+    expect_identical(names(object)[[1L]], "ENST00000456328.2")
     expect_identical(
         object = lapply(mcols(object), class),
         expected = list(
@@ -89,14 +115,20 @@ test_that("GFF3 transcripts", {
             "ccdsId" = Rle,
             "geneBiotype" = Rle,
             "geneId" = Rle,
+            "geneIdNoVersion" = Rle,
+            "geneIdVersion" = Rle,
             "geneName" = Rle,
             "havanaGene" = Rle,
             "havanaTranscript" = Rle,
             "level" = Rle,
+            "ont" = "CompressedCharacterList",
             "proteinId" = Rle,
             "source" = Rle,
+            "tag" = "CompressedCharacterList",
             "txBiotype" = Rle,
             "txId" = Rle,
+            "txIdNoVersion" = Rle,
+            "txIdVersion" = Rle,
             "txName" = Rle,
             "txSupportLevel" = Rle,
             "type" = Rle
