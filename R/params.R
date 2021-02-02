ccd12642dd874745e2b25e5b147bd81e2227b8a4
Michael Steinbaugh <mike@steinbaugh.com>
2021-02-02 07:33:39 -0500

    Update documentation

--- R/params.R
+++ R/params.R
@@ -2,6 +2,11 @@
 #' @inherit AcidRoxygen::params return title
 #' @keywords internal
 #'
+#' @param ignoreVersion `logical(1)`.
+#'   Ignore identifier (e.g. transcript, gene) versions.
+#'   When applicable, the identifier containing version numbers will be stored
+#'   in `txIdVersion` and `geneIdVersion`, and the variants without versions
+#'   will be stored in `txId`, `txIdNoVersion`, `geneId`, and `geneIdNoVersion`.
 #' @param synonyms `logical(1)`.
 #'   Include gene synonyms.
 #'   Queries the Ensembl web server, and is CPU intensive.
