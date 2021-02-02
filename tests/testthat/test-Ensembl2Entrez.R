e7b48f553d0a0614fe9072c898ee7103ac975aec
Michael Steinbaugh <mike@steinbaugh.com>
2021-02-02 17:40:36 -0500

    Fix unit test

--- tests/testthat/test-Ensembl2Entrez.R
+++ tests/testthat/test-Ensembl2Entrez.R
@@ -50,7 +50,8 @@ test_that("character", {
 
 test_that("GRanges", {
     for (format in formats) {
-        object <- Ensembl2Entrez(object = rowRanges(rse), format = format)
+        object <- GRanges
+        object <- Ensembl2Entrez(object = object, format = format)
         expect_s4_class(object, "Ensembl2Entrez")
     }
 })
