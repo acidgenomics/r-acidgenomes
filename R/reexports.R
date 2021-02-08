c242125da30e4c64be1cc6e12085675c65fc4339
Michael Steinbaugh <mike@steinbaugh.com>
2021-02-08 15:56:43 -0500

    Update reexports

--- R/reexports.R
+++ R/reexports.R
@@ -1,3 +1,19 @@
 #' @importFrom AcidBase pasteURL
 #' @export
 AcidBase::pasteURL
+
+
+
+#' @importFrom GenomicRanges GRanges
+#' @export
+GenomicRanges::GRanges
+
+#' @importFrom GenomicRanges GRangesList
+#' @export
+GenomicRanges::GRangesList
+
+
+
+#' @importFrom IRanges IRanges
+#' @export
+IRanges::IRanges
