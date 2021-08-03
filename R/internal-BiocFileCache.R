506f227a4a299823734140813a3da50afb9dd431
Michael Steinbaugh <mike@steinbaugh.com>
2021-08-03 13:24:22 -0400

    Parameterize this function

--- R/internal-BiocFileCache.R
+++ R/internal-BiocFileCache.R
@@ -1,23 +1,32 @@
 #' Dynamically cache a URL into package or return local file
 #'
-#' @note Updated 2021-03-03.
+#' @note Updated 2021-08-03.
 #' @note R won't let you name the function `.cache`.
 #' @noRd
 #'
-#' @return `character(1)`.
-#'   Local file path
+#' @param file `character`.
+#'   URLs and/or local files.
+#'
+#' @return `character`.
+#'   Local file paths.
 #'
 #' @examples
 #' file <- pasteURL(AcidGenomesTestsURL, "ensembl.gtf")
 #' tmpfile <- .cacheIt(file)
 #' print(tmpfile)
 .cacheIt <- function(file) {
-    assert(isString(file))
-    if (isAURL(file)) {
-        x <- cacheURL(url = file, pkg = .pkgName)
-    } else {
-        x <- file
-    }
-    assert(isAFile(x))
-    x
+    vapply(
+        X = file,
+        FUN = function(file) {
+            if (isAURL(file)) {
+                x <- cacheURL(url = file, pkg = .pkgName)
+            } else {
+                x <- file
+            }
+            assert(isAFile(x))
+            x
+        },
+        FUN.VALUE = character(1L),
+        USE.NAMES = FALSE
+    )
 }
