77a1552ad44ffc87013b2b208c53b63423af0b0d
Michael Steinbaugh <mike@steinbaugh.com>
2021-08-03 13:35:59 -0400

    Add documentation for "cache" argument

--- R/package.R
+++ R/package.R
@@ -33,3 +33,21 @@
 #' @importFrom stringr boundary str_extract str_match str_split_fixed
 #' @importFrom syntactic camelCase kebabCase makeNames snakeCase upperCamelCase
 "_PACKAGE"
+
+
+
+#' @name params
+#' @inherit AcidRoxygen::params return title
+#' @keywords internal
+#'
+#' @param cache `logical(1)`.
+#'   Cache URLs locally, using BiocFileCache internally.
+#' @param ignoreVersion `logical(1)`.
+#'   Ignore identifier (e.g. transcript, gene) versions.
+#'   When applicable, the identifier containing version numbers will be stored
+#'   in `txIdVersion` and `geneIdVersion`, and the variants without versions
+#'   will be stored in `txId`, `txIdNoVersion`, `geneId`, and `geneIdNoVersion`.
+#' @param synonyms `logical(1)`.
+#'   Include gene synonyms.
+#'   Queries the Ensembl web server, and is CPU intensive.
+NULL
