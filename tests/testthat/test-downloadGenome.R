0e6f1846eae15d57d90e4ec0d7134e9177b66824
Michael Steinbaugh <mike@steinbaugh.com>
2021-08-03 14:03:14 -0400

    Put genome downloads under regular code coverage

---
+++ tests/testthat/test-downloadGenome.R
@@ -0,0 +1,74 @@
+context("downloadGenome")
+
+test_that("downloadEnsemblGenome", {
+    info <- downloadEnsemblGenome(
+        organism = "Homo sapiens",
+        cache = TRUE
+    )
+    outputDir <- info[["args"]][["outputDir"]]
+    expect_true(dir.exists(outputDir))
+    expect_true(all(file.exists(file.path(
+        outputDir,
+        c(
+            "annotation.gff3.gz",
+            "annotation.gtf.gz",
+            "genome.fa.gz",
+            "metadata.rds",
+            "transcriptome.fa.gz",
+            "tx2gene.csv.gz"
+        )
+    ))))
+    if (dir.exists(outputDir)) {
+        unlink(outputDir, recursive = TRUE)
+    }
+})
+
+test_that("downloadGencodeGenome", {
+    info <- downloadGencodeGenome(
+        organism = "Homo sapiens",
+        cache = TRUE
+    )
+    outputDir <- info[["args"]][["outputDir"]]
+    expect_true(dir.exists(outputDir))
+    expect_true(all(file.exists(file.path(
+        outputDir,
+        c(
+            "annotation.gff3.gz",
+            "annotation.gtf.gz",
+            "genome.fa.gz",
+            "metadata.rds",
+            "transcriptome.fa.gz",
+            "tx2gene.csv.gz"
+        )
+    ))))
+    if (dir.exists(outputDir)) {
+        unlink(outputDir, recursive = TRUE)
+    }
+})
+
+test_that("downloadRefSeqGenome", {
+    info <- downloadRefSeqGenome(
+        organism = "Homo sapiens",
+        taxonomicGroup = "vertebrate_mammalian",
+        cache = TRUE
+    )
+    outputDir <- info[["args"]][["outputDir"]]
+    expect_true(dir.exists(outputDir))
+    expect_true(all(file.exists(file.path(
+        outputDir,
+        c(
+            "annotation.gff3.gz",
+            "annotation.gtf.gz",
+            "genome.fa.gz",
+            "metadata.rds",
+            "transcriptome.fa.gz"
+            ## NOTE Check for this in a future update.
+            ## > "tx2gene.csv.gz"
+        )
+    ))))
+    if (dir.exists(outputDir)) {
+        unlink(outputDir, recursive = TRUE)
+    }
+})
+
+## FIXME Need to add UCSC genome coverage.
