5f567e396557869038cfb72221e68bfa023fa611
Michael Steinbaugh <mike@steinbaugh.com>
2021-08-03 13:21:01 -0400

    Add BiocFileCache support

--- R/internal.R
+++ R/internal.R
@@ -1,31 +1,51 @@
+## FIXME Add file caching option.
+## FIXME Call to `.cacheIt` here when `cache = TRUE`.
+
 #' Download multiple genome files in a single call
 #'
-#' @note Updated 2021-01-07.
+#' @note Updated 2021-08-03.
 #' @noRd
 #'
 #' @return `character`
 #'   Destination files.
-.downloadURLs <- function(urls, outputDir) {
+.downloadURLs <- function(
+    urls,
+    outputDir,
+    cache = FALSE
+) {
     assert(
         allAreURLs(urls),
-        isString(outputDir)
+        isString(outputDir),
+        isFlag(cache)
     )
     outputDir <- initDir(outputDir)
-    destfiles <- vapply(
+    destFiles <- vapply(
         X = urls,
         FUN = function(url) {
             file.path(outputDir, basename(url))
         },
-        FUN.VALUE = character(1L)
+        FUN.VALUE = character(1L),
+        USE.NAMES = TRUE
     )
-    assert(identical(names(urls), names(destfiles)))
-    out <- mapply(
-        url = urls,
-        destfile = destfiles,
-        FUN = download,
-        SIMPLIFY = TRUE,
-        USE.NAMES = FALSE
-    )
+    assert(identical(names(urls), names(destFiles)))
+    if (isTRUE(cache)) {
+        cacheFiles <- .cacheIt(file = urls)
+        file.copy(
+            from = cacheFiles,
+            to = destFiles,
+            overwrite = TRUE,
+            recursive = FALSE
+        )
+    } else {
+        mapply(
+            url = urls,
+            destfile = destFiles,
+            FUN = download,
+            SIMPLIFY = TRUE,
+            USE.NAMES = FALSE
+        )
+    }
+    out <- destFiles
     names(out) <- names(urls)
     assert(allAreFiles(out))
     invisible(out)
