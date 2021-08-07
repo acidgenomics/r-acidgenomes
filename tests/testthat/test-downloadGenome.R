1e9487cee0b893904ec8df80e90ace825aa1ac2a
Michael Steinbaugh <mike@steinbaugh.com>
2021-08-07 08:49:31 -0400

    Improve code coverage

--- tests/testthat/test-downloadGenome.R
+++ tests/testthat/test-downloadGenome.R
@@ -83,14 +83,14 @@ test_that("downloadGencodeGenome", {
     )
     tx2gene <- import(
         file = file.path(outputDir, "tx2gene.csv.gz"),
-        colnames = FALSE
+        colnames = c("txId", "geneId")
     )
     expect_identical(nrow(tx2gene), 237012L)
     expect_identical(
         object = as.data.frame(tx2gene)[1L, , drop = TRUE],
         expected = list(
-            "V1" = "ENST00000000233.10",
-            "V2" = "ENSG00000004059.11"
+            "txId" = "ENST00000000233.10",
+            "geneId" = "ENSG00000004059.11"
         )
     )
     if (dir.exists(outputDir)) {
@@ -102,6 +102,7 @@ test_that("downloadRefSeqGenome", {
     info <- downloadRefSeqGenome(
         organism = "Homo sapiens",
         taxonomicGroup = "vertebrate_mammalian",
+        genomeBuild = "GCF_000001405.39_GRCh38.p13",
         cache = TRUE
     )
     outputDir <- info[["args"]][["outputDir"]]
@@ -111,12 +112,35 @@ test_that("downloadRefSeqGenome", {
         c(
             "annotation.gff3.gz",
             "annotation.gtf.gz",
+            "genes.rds",
             "genome.fa.gz",
             "metadata.rds",
             "transcriptome.fa.gz",
-            "tx2gene.csv.gz"
+            "transcripts.rds",
+            "tx2gene.csv.gz",
+            "tx2gene.rds"
         )
     ))))
+    aatfExpected <- data.frame(
+        "txId" = c(
+            "NM_012138.4",
+            "XM_011524611.2",
+            "XR_934439.3"
+        ),
+        "geneId" = rep("AATF", 3L)
+    )
+    tx2gene <- import(file.path(outputDir, "tx2gene.rds"))
+    tx2gene <- as.data.frame(tx2gene)
+    aatfCurrent <- tx2gene[tx2gene[, 2L] == "AATF", ]
+    rownames(aatfCurrent) <- NULL
+    expect_identical(aatfCurrent, aatfExpected)
+    tx2gene <- import(
+        file = file.path(outputDir, "tx2gene.csv.gz"),
+        colnames = c("txId", "geneId")
+    )
+    aatfCurrent <- tx2gene[tx2gene[, 2L] == "AATF", ]
+    rownames(aatfCurrent) <- NULL
+    expect_identical(aatfCurrent, aatfExpected)
     if (dir.exists(outputDir)) {
         unlink(outputDir, recursive = TRUE)
     }
