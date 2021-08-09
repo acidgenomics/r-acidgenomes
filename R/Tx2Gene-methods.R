d6a05bb53512281be5412a51c286ea3efc830328
Michael Steinbaugh <mike@steinbaugh.com>
2021-08-09 18:10:19 -0400

    Add `removeNA` mode and harden default handling

--- R/Tx2Gene-methods.R
+++ R/Tx2Gene-methods.R
@@ -2,9 +2,14 @@
 #' @inherit Tx2Gene-class title description return
 #'
 #' @note No attempt is made to arrange the rows by transcript identifier.
-#' @note Updated 2021-08-03.
+#' @note Updated 2021-08-09.
 #'
 #' @inheritParams AcidRoxygen::params
+#' @param removeNA `logical(1)`.
+#'   Remove elements that don't contain clear mappings (e.g. gene identifier
+#'   is `NA`). Calls `complete.cases` internally.
+#'   If set `FALSE`, the function will intentionally error if any incomplete
+#'   cases are detected.
 #'
 #' @seealso [makeTx2Gene()].
 #'
@@ -26,33 +31,36 @@ NULL
 
 
 
-## Updated 2020-01-07.
+## Updated 2021-08-09.
 `Tx2Gene,matrix` <-  # nolint
-    function(object) {
+    function(object, ...) {
         assert(
             is.character(object),
             identical(ncol(object), 2L)
         )
         object <- as.data.frame(object, stringsAsFactors = FALSE)
-        Tx2Gene(object)
+        Tx2Gene(object, ...)
     }
 
 
 
-## Updated 2020-01-29.
+## Updated 2021-08-09.
 `Tx2Gene,data.frame` <-  # nolint
-    function(object) {
+    function(object, ...) {
         assert(identical(ncol(object), 2L))
         colnames(object) <- c("txId", "geneId")
         object <- as(object, "DataFrame")
-        Tx2Gene(object)
+        Tx2Gene(object, ...)
     }
 
 
 
-## Updated 2021-08-03.
+## Updated 2021-08-09.
 `Tx2Gene,DataFrame` <-  # nolint
-    function(object) {
+    function(
+        object,
+        removeNA = FALSE
+    ) {
         assert(hasColnames(object))
         meta <- metadata(object)
         cols <- c("txId", "geneId")
@@ -71,24 +79,28 @@ NULL
         object <- object[, cols, drop = FALSE]
         object <- decode(object)
         assert(allAreAtomic(object))
-        object <- unique(object)
-        keep <- complete.cases(object)
-        if (!all(keep)) {
-            meta[["dropped"]] <- which(!keep)
-            n <- sum(!keep)
-            alertWarning(sprintf(
-                "Dropping %d %s without defined transcript-to-gene mapping.",
-                n,
-                ngettext(
-                    n = n,
-                    msg1 = "identifier",
-                    msg2 = "identifiers"
-                )
-            ))
-            object <- object[keep, , drop = FALSE]
-            assert(hasRows(object))
+        if (isTRUE(removeNA)) {
+            keep <- complete.cases(object)
+            if (!all(keep)) {
+                meta[["dropped"]] <- which(!keep)
+                n <- sum(!keep)
+                alertWarning(sprintf(
+                    "Dropping %d %s without transcript-to-gene mapping.",
+                    n,
+                    ngettext(
+                        n = n,
+                        msg1 = "identifier",
+                        msg2 = "identifiers"
+                    )
+                ))
+                object <- object[keep, , drop = FALSE]
+            }
         }
-        assert(hasNoDuplicates(object[[cols[[1L]]]]))
+        assert(
+            hasRows(object),
+            hasNoDuplicates(object[[cols[[1L]]]]),
+            all(complete.cases(object))
+        )
         object <- object[order(object), , drop = FALSE]
         metadata(object) <- meta
         new(Class = "Tx2Gene", object)
@@ -96,24 +108,24 @@ NULL
 
 
 
-## Updated 2021-01-29.
+## Updated 2021-08-09.
 `Tx2Gene,GRanges` <-  # nolint
-    function(object) {
+    function(object, ...) {
         df <- as(object, "DataFrame")
         metadata(df) <- metadata(object)
-        Tx2Gene(df)
+        Tx2Gene(df, ...)
     }
 
 
 
-## Updated 2021-08-03.
+## Updated 2021-08-09.
 `Tx2Gene,GRangesList` <-  # nolint
-    function(object) {
+    function(object, ...) {
         x <- as(object, "GRangesList")
         x <- unname(x)
         gr <- unlist(x)
         assert(is(gr, "GRanges"))
-        Tx2Gene(gr)
+        Tx2Gene(gr, ...)
     }
 
 
