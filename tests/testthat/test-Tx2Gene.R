813ef6ada2352abc4b49289e05d386e30ce64a76
Michael Steinbaugh <mike@steinbaugh.com>
2021-08-10 12:54:56 -0400

    Improve code coverage of Tx2Gene return

--- tests/testthat/test-Tx2Gene.R
+++ tests/testthat/test-Tx2Gene.R
@@ -4,21 +4,80 @@
 
 context("Tx2Gene")
 
-test_that("DataFrame", {
+test_that("Complete cases", {
     df <- DataFrame(
         "txId" = c(
-            "ENST00000635602.1",
-            "ENST00000635506.1"
+            "transcript1",
+            "transcript2",
+            "transcript3",
+            "transcript4",
+            NA_character_
         ),
         "geneId" = c(
-            "ENSG00000283061.1",
-            "ENSG00000283061.1"
-        )
+            "gene1",
+            "gene1",
+            "gene2",
+            NA_character_,
+            "gene3"
+        ),
+        row.names = LETTERS[seq_len(5L)]
     )
     object <- Tx2Gene(df)
     expect_s4_class(object, "Tx2Gene")
+    expected <- DataFrame(
+        "txId" = c(
+            "transcript1",
+            "transcript2",
+            "transcript3"
+        ),
+        "geneId" = c(
+            "gene1",
+            "gene1",
+            "gene2"
+        ),
+        row.names = LETTERS[seq_len(3L)]
+    )
+    metadata(expected) <- list("dropped" = c("D" = 4L, "E" = 5L))
+    expected <- new(Class = "Tx2Gene", expected)
+    expect_identical(object, expected)
     expect_output(
         object = summary(object),
-        regexp = "genes: 1"
+        regexp = "genes: 2"
     )
+})
+
+test_that("Ensure transcripts return sorted", {
+    df <- DataFrame(
+        "txId" = c(
+            "transcript2",
+            "transcript1",
+            "transcript4",
+            "transcript3"
+        ),
+        "geneId" = c(
+            "gene1",
+            "gene2",
+            "gene1",
+            "gene2"
+        ),
+        row.names = LETTERS[seq_len(4L)]
+    )
+    object <- Tx2Gene(df)
+    expected <- DataFrame(
+        "txId" = c(
+            "transcript1",
+            "transcript2",
+            "transcript3",
+            "transcript4"
+        ),
+        "geneId" = c(
+            "gene2",
+            "gene1",
+            "gene2",
+            "gene1"
+        ),
+        row.names = c("B", "A", "D", "C")
+    )
+    expected <- new(Class = "Tx2Gene", expected)
+    expect_identical(object, expected)
 })
