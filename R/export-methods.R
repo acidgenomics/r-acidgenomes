54da92168f6a1215c5b3566219827f065f5124a7
Michael Steinbaugh <mike@steinbaugh.com>
2021-10-21 11:26:10 -0400

    Draft update to export handling without colnames

--- R/export-methods.R
+++ R/export-methods.R
@@ -1,4 +1,5 @@
 ## FIXME We need to provide non-breaking support for "file" argument here.
+## FIXME pipette colnames = FALSE may not be working the way we want here...
 
 
 
@@ -27,12 +28,14 @@
 #' )
 #' con <- file.path(tempdir(), "tx2gene.csv")
 #' export(object = object, con = con)
+#' x <- readLines(con, n = 4L)
+#' print(x)
 #' file.remove(con)
 NULL
 
 
 
-## Updated 2021-09-24.
+## Updated 2021-10-21.
 `export,Tx2Gene` <-  # nolint
     function(
         object,
@@ -52,6 +55,7 @@ NULL
             object = df,
             con = con,
             format = format,
+            ## FIXME This setting isn't propagating the way we want downstream...
             colnames = FALSE,
             ...
         )
