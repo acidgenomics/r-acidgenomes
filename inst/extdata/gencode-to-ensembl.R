e8acc805fc58b70ec43ead47448cdd574db04125
Michael Steinbaugh <mike@steinbaugh.com>
2022-02-07 10:22:10 -0500

    Update organism detection

--- inst/extdata/gencode-to-ensembl.R
+++ inst/extdata/gencode-to-ensembl.R
@@ -1,5 +1,5 @@
 ## https://ftp.ebi.ac.uk/pub/databases/gencode/Gencode_human/_README.TXT
-library(basejump)
+library(pipette)
 object <- import("gencode-to-ensembl.csv")
 object <- as(object, "DataFrame")
 saveRDS(object = object, file = "gencode-to-ensembl.rds")
