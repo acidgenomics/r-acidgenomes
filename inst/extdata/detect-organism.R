b9b849d5fc4f140527ea58a1636002715c0e3111
Michael Steinbaugh <mike@steinbaugh.com>
2022-05-24 08:53:45 -0400

    Update internal data

--- inst/extdata/detect-organism.R
+++ inst/extdata/detect-organism.R
@@ -1,4 +1,8 @@
-library(pipette)
+## nolint start
+suppressPackageStartupMessages({
+    library(pipette)
+})
+## nolint end
 object <- import("detect-organism.csv")
 object <- as(object, "DataFrame")
 saveRDS(object = object, file = "detect-organism.rds")
