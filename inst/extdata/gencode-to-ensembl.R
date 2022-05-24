f682cbbe726d6c1bb5c5ca5f215f680218ef7019
Michael Steinbaugh <mike@steinbaugh.com>
2022-05-24 08:53:45 -0400

    Update internal data

--- inst/extdata/gencode-to-ensembl.R
+++ inst/extdata/gencode-to-ensembl.R
@@ -1,5 +1,9 @@
 ## https://ftp.ebi.ac.uk/pub/databases/gencode/Gencode_human/_README.TXT
-library(pipette)
+## nolint start
+suppressPackageStartupMessages({
+    library(pipette)
+})
+## nolint end
 object <- import("gencode-to-ensembl.csv")
 object <- as(object, "DataFrame")
 saveRDS(object = object, file = "gencode-to-ensembl.rds")
