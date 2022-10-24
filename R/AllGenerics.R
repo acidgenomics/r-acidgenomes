39daf36c8654f407a1cf6c137817bb417549a6ae
Michael Steinbaugh <mike@steinbaugh.com>
2022-10-24 15:23:10 -0400

    Consolidate importFrom calls into imports.R

--- R/AllGenerics.R
+++ R/AllGenerics.R
@@ -1,72 +1,59 @@
-## FIXME Migrate the imports to "imports.R" file.
-
-
 #' @rdname Ensembl2Entrez
 #' @name Ensembl2Entrez
-#' @importFrom AcidGenerics Ensembl2Entrez
 #' @usage Ensembl2Entrez(object, ...)
 #' @export
 NULL
 
 #' @rdname Entrez2Ensembl
 #' @name Entrez2Ensembl
-#' @importFrom AcidGenerics Entrez2Ensembl
 #' @usage Entrez2Ensembl(object, ...)
 #' @export
 NULL
 
 #' @rdname Gene2Symbol
 #' @name Gene2Symbol
-#' @importFrom AcidGenerics Gene2Symbol
 #' @usage Gene2Symbol(object, ...)
 #' @export
 NULL
 
 #' @rdname Tx2Gene
 #' @name Tx2Gene
-#' @importFrom AcidGenerics Tx2Gene
 #' @usage Tx2Gene(object, ...)
 #' @export
 NULL
 
 #' @rdname export
 #' @name export
-#' @importFrom pipette export
 #' @usage export(object, con, format, ...)
 #' @export
 NULL
 
 #' @rdname organism
 #' @name organism
-#' @importFrom BiocGenerics organism
 #' @usage organism(object)
 #' @export
 NULL
 
 #' @rdname organism
 #' @name organism<-
-#' @importFrom BiocGenerics organism<-
 #' @usage organism(object) <- value
 #' @export
 NULL
 
 #' @rdname stripGeneVersions
 #' @name stripGeneVersions
-#' @importFrom AcidGenerics stripGeneVersions
 #' @usage stripGeneVersions(object, ...)
 #' @export
 NULL
 
 #' @rdname stripTranscriptVersions
 #' @name stripTranscriptVersions
-#' @importFrom AcidGenerics stripTranscriptVersions
 #' @usage stripTranscriptVersions(object, ...)
 #' @export
 NULL
 
 #' @rdname summary
 #' @name summary
-#' @importFrom S4Vectors summary
 #' @usage summary(object, ...)
 #' @export
 NULL
