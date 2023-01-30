d499c47cb3d1bfeca0d2137c0ae337419bb74b4a
Michael Steinbaugh <mike@steinbaugh.com>
2023-01-30 15:55:07 -0500

    Improve coverage and documentation

--- tests/testthat/test-mapGencodeToEnsembl.R
+++ tests/testthat/test-mapGencodeToEnsembl.R
@@ -1,6 +1,10 @@
 test_that("mapGencodeToEnsembl", {
     expect_identical(
-        object = mapGencodeToEnsembl(39L),
-        expected = 105L
+        object = mapGencodeToEnsembl(42L),
+        expected = 108L
+    )
+    expect_identical(
+        object = mapGencodeToEnsembl("M31"),
+        expected = 108L
     )
 })
