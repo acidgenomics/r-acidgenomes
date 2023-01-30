c89650aed05a9f0f5c90fb6c6f756acb9c6bdc24
Michael Steinbaugh <mike@steinbaugh.com>
2023-01-30 16:55:16 -0500

    Improve coverage of expected values

--- tests/testthat/test-geneSynonyms.R
+++ tests/testthat/test-geneSynonyms.R
@@ -1,7 +1,14 @@
 test_that("geneSynonyms", {
-    organisms <- c("Homo sapiens", "Mus musculus")
-    for (organism in organisms) {
-        object <- geneSynonyms(organism = organism)
-        expect_s4_class(object, "DataFrame")
-    }
+    object <- geneSynonyms(organism = "Homo sapiens")
+    expect_s4_class(object, "DataFrame")
+    expect_identical(
+        object = object["1", "geneSynonyms"],
+        expected = CharacterList(c("A1B", "ABG", "GAB", "HYST2477"))
+    )
+    object <- geneSynonyms(organism = "Mus musculus")
+    expect_s4_class(object, "DataFrame")
+    expect_identical(
+        object = object["11287", "geneSynonyms"],
+        expected = CharacterList(c("A1m", "A2m", "MAM"))
+    )
 })
