d845f13bbca1f40fc0c07b2bbc9b0b4d995e0022
Michael Steinbaugh <mike@steinbaugh.com>
2023-01-30 16:57:21 -0500

    Improve coverage, checking expected values

--- tests/testthat/test-HGNC2Ensembl.R
+++ tests/testthat/test-HGNC2Ensembl.R
@@ -1,4 +1,12 @@
 test_that("HGNC2Ensembl", {
     object <- HGNC2Ensembl()
     expect_s4_class(object, "HGNC2Ensembl")
+    expect_identical(
+        object = as.data.frame(object)["5", ],
+        expected = data.frame(
+            "hgncId" = 5L,
+            "ensemblId" = "ENSG00000121410",
+            row.names = "5"
+        )
+    )
 })
