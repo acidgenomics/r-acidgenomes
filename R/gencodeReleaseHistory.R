2a0dd260b0688e72068e0122ec49fabf8c2212c8
Michael Steinbaugh <mike@steinbaugh.com>
2023-02-10 16:50:20 -0500

    Add gencodeReleaseHistory, which uses rvest to extract metadata automatically

---
+++ R/gencodeReleaseHistory.R
@@ -0,0 +1,35 @@
+#' GENCODE release history
+#'
+#' @export
+#' @note Updated 2023-02-10.
+#'
+#' @inheritParams AcidRoxygen::params
+#'
+#' @return `DataFrame`.
+#'
+#' @examples
+#' object <- gencodeReleaseHistory(organism = "Homo sapiens")
+gencodeReleaseHistory <-
+    function(organism) {
+        assert(requireNamespaces("rvest"))
+        organism <- match.arg(
+            arg = organism,
+            choices = c("Homo sapiens", "Mus musculus")
+        )
+        orgShort <- switch(
+            EXPR = organism,
+            "Homo sapiens" = "human",
+            "Mus musculus" = "mouse"
+        )
+        url <- pasteURL(
+            "www.gencodegenes.org",
+            orgShort,
+            "releases.html",
+            protocol = "https"
+        )
+        html <- rvest::read_html(url)
+        table <- rvest::html_table(html)
+        df <- as(table[[1L]], "DataFrame")
+        colnames(df) <- camelCase(colnames(df), strict = TRUE)
+        df
+    }
