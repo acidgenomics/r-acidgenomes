825d801c765012686ad3535764520df4267be945
Michael Steinbaugh <mike@steinbaugh.com>
2023-03-03 07:20:43 -0500

    Initial commit

---
+++ R/mapGeneNamesToEnsembl.R
@@ -0,0 +1,15 @@
+## FIXME Use NCBI aliases for this.
+
+
+
+#' Map gene names to Ensembl
+#'
+#' @export
+#' @note Updated 2023-03-03.
+#'
+#' @inheritParams AcidRoxygen::params
+#'
+#' @noRd
+mapGeneNamesToEnsembl <-
+    function(genes, organism) {
+    }
