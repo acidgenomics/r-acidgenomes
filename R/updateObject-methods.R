8ad00c1fa240e93d063836eb58f623c153df1357
Michael Steinbaugh <mike@steinbaugh.com>
2023-04-13 15:23:27 -0400

    Work toward update support of EnsemblGenes and GencodeGenes

--- R/updateObject-methods.R
+++ R/updateObject-methods.R
@@ -1,3 +1,33 @@
+## FIXME Rename `entrezId` to `ncbiGeneId`.
 ## FIXME Add updateObject support for EnsemblGenes.
-## Rename `entrezId` to `ncbiGeneId`.
 ## FIXME Also add updateObject support for EnsemblTranscripts here.
+## FIXME Add update support for GencodeGenes and GencodeTranscripts.
+
+
+
+#' Update object
+#'
+#' @name updateObject
+#' @author Michael Steinbaugh
+#' @note Updated 2023-03-07.
+NULL
+
+
+
+## Updated 2023-04-13.
+`updateObject,EnsemblGenes` <- # nolint
+    function(object, ..., verbose = FALSE) {
+        stop("FIXME")
+
+        new(Class = "EnsemblGenes", object)
+    }
+
+
+
+#' @rdname updateObject
+#' @export
+setMethod(
+    f = "updateObject",
+    signature = signature(object = "EnsemblGenes"),
+    definition = `updateObject,EnsemblGenes`
+)
