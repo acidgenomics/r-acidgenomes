4eb586d44c4bf96d579edf3be1db37a88d87308c
Michael Steinbaugh <mike@steinbaugh.com>
2023-04-13 15:39:20 -0400

    Add support for updating legacy objects

--- R/updateObject-methods.R
+++ R/updateObject-methods.R
@@ -15,12 +15,39 @@ NULL
 
 
 ## Updated 2023-04-13.
-`updateObject,EnsemblGenes` <- # nolint
+.updateGRanges <-
     function(object, ..., verbose = FALSE) {
-        stop("FIXME")
-
-        new(Class = "EnsemblGenes", object)
+        if (isSubset("entrezId", colnames(mcols(object)))) {
+            if (isTRUE(verbose)) {
+                alert(sprintf(
+                    "Renaming {.var %s} to {.var %s}.",
+                    "entrezId", "ncbiGeneId"
+                ))
+            }
+            mcols(object)[["ncbiGeneId"]] <- mcols(object)[["entrezId"]]
+            mcols(object)[["entrezId"]] <- NULL
+        }
+        validObject(object)
+        object
     }
+
+
+
+## Updated 2023-04-13.
+`updateObject,EnsemblGenes` <- # nolint
+    .updateGRanges
+
+## Updated 2023-04-13.
+`updateObject,EnsemblTranscripts` <- # nolint
+    .updateGRanges
+
+## Updated 2023-04-13.
+`updateObject,GencodeGenes` <- # nolint
+    .updateGRanges
+
+## Updated 2023-04-13.
+`updateObject,GencodeTranscripts` <- # nolint
+    .updateGRanges
 
 
 
@@ -30,4 +57,28 @@ setMethod(
     f = "updateObject",
     signature = signature(object = "EnsemblGenes"),
     definition = `updateObject,EnsemblGenes`
+)
+
+#' @rdname updateObject
+#' @export
+setMethod(
+    f = "updateObject",
+    signature = signature(object = "EnsemblTranscripts"),
+    definition = `updateObject,EnsemblTranscripts`
+)
+
+#' @rdname updateObject
+#' @export
+setMethod(
+    f = "updateObject",
+    signature = signature(object = "GencodeGenes"),
+    definition = `updateObject,GencodeGenes`
+)
+
+#' @rdname updateObject
+#' @export
+setMethod(
+    f = "updateObject",
+    signature = signature(object = "GencodeTranscripts"),
+    definition = `updateObject,GencodeTranscripts`
 )
