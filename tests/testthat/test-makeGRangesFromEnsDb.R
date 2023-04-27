06a3dedd11863ff9abde9e507810a76539e0414d
Michael Steinbaugh <mike@steinbaugh.com>
2023-04-27 08:37:42 -0400

    Update unit tests

--- tests/testthat/test-makeGRangesFromEnsDb.R
+++ tests/testthat/test-makeGRangesFromEnsDb.R
@@ -1,6 +1,4 @@
-## Conditionally test if optional EnsDb.Hsapiens.v75 package is installed.
 pkg <- "EnsDb.Hsapiens.v75"
-skip_if_not_installed(pkg)
 
 test_that("Genes", {
     x <- makeGRangesFromEnsDb(object = pkg, level = "genes")
