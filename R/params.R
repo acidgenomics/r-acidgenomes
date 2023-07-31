ce4e4735ef2aeb11fd75f8973ff76625eef0e10d
Michael Steinbaugh <mike@steinbaugh.com>
2023-07-31 17:35:12 -0400

    Update params

--- R/params.R
+++ R/params.R
@@ -5,6 +5,9 @@
 #' @param cache `logical(1)`.
 #' Cache URLs locally, using BiocFileCache internally.
 #'
+#' @param extraMcols `logical(1)`.
+#' Include extra metadata columns (e.g. `"broadClass"`).
+#'
 #' @param ignoreVersion `logical(1)`.
 #' Ignore identifier (e.g. transcript, gene) versions.
 #' When applicable, the identifier containing version numbers will be stored
