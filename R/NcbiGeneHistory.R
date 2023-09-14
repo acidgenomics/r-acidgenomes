af2853e9b2fcbe1b94a549f84d5470f81f13268b
Michael Steinbaugh <mike@steinbaugh.com>
2023-09-14 18:57:25 -0400

    Prepare initial version of NcbiGeneHistory

--- R/NcbiGeneHistory.R
+++ R/NcbiGeneHistory.R
@@ -1,37 +1,23 @@
-## FIXME Make this more user-friendly by supporting organism-to-taxonomy.
-## FIXME Use .mapOrganismToNcbiTaxId to map organism here.
-
-
-
 #' NCBI gene history
 #'
 #' @export
 #' @note Updated 2023-09-14.
 #'
-#' @param `integer(1)`.
-#' NCBI taxonomy identifier.
-#' For *Homo sapiens*, use `9606L`.
+#' @inheritParams AcidRoxygen::params
 #'
 #' @return `DFrame`.
 #'
 #' @examples
 #' ## Homo sapiens.
-#' df <- NcbiGeneHistory(taxonomyId = 9606L)
+#' df <- NcbiGeneHistory(organism = "Homo sapiens")
 NcbiGeneHistory <- function(organism) {
     assert(isOrganism(organism))
-    ## FIXME Need to map organism to NCBI taxonomy.
+    taxId <- .mapOrganismToNcbiTaxId(organism)
     url <- pasteURL(
-        "ftp.ncbi.nih.gov",
-        "gene",
-        "DATA",
-        "gene_history.gz",
+        "ftp.ncbi.nih.gov", "gene", "DATA", "gene_history.gz",
         protocol = "ftp"
     )
-    df <- import(
-        con = .cacheIt(url),
-        format = "tsv",
-        engine = "readr"
-    )
+    df <- import(con = .cacheIt(url), format = "tsv", engine = "readr")
     df <- as(df, "DFrame")
     colnames(df) <- camelCase(colnames(df))
     assert(identical(
@@ -44,11 +30,15 @@ NcbiGeneHistory <- function(organism) {
             "discontinueDate"
         )
     ))
-    colnames(df)[colnames(df) == "xTaxId"] <- "taxonomyId"
+    keep <- df[["xTaxId"]] == taxId
+    df[["xTaxId"]] <- NULL
     df <- df[keep, , drop = FALSE]
     df[["geneId"]] <- as.integer(df[["geneId"]])
     df[["discontinuedGeneId"]] <- as.integer(df[["discontinuedGeneId"]])
-    df[["discontinueDate"]] <-
-        as.Date(x = as.character(xxx), tryFormats = "%Y%m%d")
+    df[["discontinueDate"]] <- as.Date(
+        x = as.character(df[["discontinueDate"]]),
+        tryFormats = "%Y%m%d"
+    )
+    df <- df[order(df), , drop = FALSE]
     df
 }
