4525e4a2c47f324f302d3a13bb84b85d9e69c19b
Michael Steinbaugh <mike@steinbaugh.com>
2023-09-26 21:45:15 -0400

    Pass lintr checks

--- R/NcbiGeneHistory.R
+++ R/NcbiGeneHistory.R
@@ -11,66 +11,67 @@
 #' ## Homo sapiens.
 #' object <- NcbiGeneHistory(organism = "Homo sapiens")
 #' print(object)
-NcbiGeneHistory <- function(organism) {
-    assert(isOrganism(organism))
-    taxId <- .mapOrganismToNcbiTaxId(organism)
-    assert(isInt(taxId))
-    url <- pasteURL(
-        "ftp.ncbi.nih.gov", "gene", "DATA", "gene_history.gz",
-        protocol = "https"
-    )
-    ## readr is much faster than base engine at parsing this file.
-    df <- import(
-        con = .cacheIt(url),
-        format = "tsv",
-        engine = ifelse(
-            test = isInstalled("readr"),
-            yes = "readr",
-            no = "base"
-        ),
-        naStrings = "-"
-    )
-    df <- as(df, "DFrame")
-    colnames(df) <- camelCase(colnames(df))
-    assert(identical(
-        x = colnames(df),
-        y = c(
-            "xTaxId",
-            "geneId",
+NcbiGeneHistory <- # nolint
+    function(organism) {
+        assert(isOrganism(organism))
+        taxId <- .mapOrganismToNcbiTaxId(organism)
+        assert(isInt(taxId))
+        url <- pasteURL(
+            "ftp.ncbi.nih.gov", "gene", "DATA", "gene_history.gz",
+            protocol = "https"
+        )
+        ## readr is much faster than base engine at parsing this file.
+        df <- import(
+            con = .cacheIt(url),
+            format = "tsv",
+            engine = ifelse(
+                test = isInstalled("readr"),
+                yes = "readr",
+                no = "base"
+            ),
+            naStrings = "-"
+        )
+        df <- as(df, "DFrame")
+        colnames(df) <- camelCase(colnames(df))
+        assert(identical(
+            x = colnames(df),
+            y = c(
+                "xTaxId",
+                "geneId",
+                "discontinuedGeneId",
+                "discontinuedSymbol",
+                "discontinueDate"
+            ),
+            hasNoDuplicates(df[["discontinuedGeneId"]])
+        ))
+        keep <- df[["xTaxId"]] == taxId
+        df[["xTaxId"]] <- NULL
+        df <- df[keep, , drop = FALSE]
+        df[["discontinueDate"]] <- sub(
+            pattern = "^([0-9]{4})([0-9]{2})([0-9]{2})$",
+            replacement = "\\1-\\2-\\3",
+            x = df[["discontinueDate"]]
+        )
+        df[["discontinueDate"]] <- as.Date(df[["discontinueDate"]])
+        df[["discontinuedGeneId"]] <- as.integer(df[["discontinuedGeneId"]])
+        df[["geneId"]] <- as.integer(df[["geneId"]])
+        j <- c(
             "discontinuedGeneId",
             "discontinuedSymbol",
-            "discontinueDate"
-        ),
-        hasNoDuplicates(df[["discontinuedGeneId"]])
-    ))
-    keep <- df[["xTaxId"]] == taxId
-    df[["xTaxId"]] <- NULL
-    df <- df[keep, , drop = FALSE]
-    df[["discontinueDate"]] <- sub(
-        pattern = "^([0-9]{4})([0-9]{2})([0-9]{2})$",
-        replacement = "\\1-\\2-\\3",
-        x = df[["discontinueDate"]]
-    )
-    df[["discontinueDate"]] <- as.Date(df[["discontinueDate"]])
-    df[["discontinuedGeneId"]] <- as.integer(df[["discontinuedGeneId"]])
-    df[["geneId"]] <- as.integer(df[["geneId"]])
-    j <- c(
-        "discontinuedGeneId",
-        "discontinuedSymbol",
-        "discontinueDate",
-        "geneId"
-    )
-    df <- df[, j, drop = FALSE]
-    i <- order(df)
-    df <- df[i, , drop = FALSE]
-    df <- encode(df)
-    rownames(df) <- df[["discontinuedGeneId"]]
-    metadata(df) <- list(
-        "date" = Sys.Date(),
-        "organism" = organism,
-        "packageVersion" = .pkgVersion,
-        "taxonomyId" = taxId,
-        "url" = url
-    )
-    new(df, Class = "NcbiGeneHistory")
-}
+            "discontinueDate",
+            "geneId"
+        )
+        df <- df[, j, drop = FALSE]
+        i <- order(df)
+        df <- df[i, , drop = FALSE]
+        df <- encode(df)
+        rownames(df) <- df[["discontinuedGeneId"]]
+        metadata(df) <- list(
+            "date" = Sys.Date(),
+            "organism" = organism,
+            "packageVersion" = .pkgVersion,
+            "taxonomyId" = taxId,
+            "url" = url
+        )
+        new(df, Class = "NcbiGeneHistory")
+    }
