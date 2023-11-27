6f4006543992c46b95c65e432e08449cfb3291d1
Michael Steinbaugh <mike@steinbaugh.com>
2023-11-27 14:57:34 -0500

    Update unit test

--- tests/testthat/test-NcbiToEnsembl.R
+++ tests/testthat/test-NcbiToEnsembl.R
@@ -2,69 +2,23 @@
 ## FIXME Need to add testing for ambiguous matches.
 
 test_that("character : 1:1 (unique)", {
-    object <- NcbiToEnsembl(
-        object = c(
-            64102L,
-            10838L,
-            7982L,
-            7105L
-        ),
-        organism = "Homo sapiens",
-        format = "1:1"
-    )
-    expect_identical(metadata(object)[["format"]], "1:1")
+    genes <- c(64102L, 10838L, 7982L, 7105L)
+    object <- NcbiToEnsembl(genes, organism = "Homo sapiens")
     expected <- DataFrame(
-        "ncbiGeneId" = c(
-            64102L,
-            10838L,
-            7982L,
-            7105L
-        ),
+        "ncbiGeneId" = genes,
         "ensemblGeneId" = c(
             "ENSG00000000005",
             "ENSG00000063587",
             "ENSG00000004866",
             "ENSG00000000003"
-        )
+        ),
+        row.names = genes
     )
     expect_identical(
         object = as.data.frame(object),
         expected = as.data.frame(expected)
     )
-})
-
-## FIXME Need to use a gene that multi-maps here.
-
-test_that("character : long (non-unique)", {
-    object <- NcbiToEnsembl(
-        object = c(
-            64102L,
-            10838L,
-            7982L,
-            7105L
-        ),
-        organism = "Homo sapiens",
-        format = "long"
-    )
-    expect_identical(metadata(object)[["format"]], "long")
-    expected <- DataFrame(
-        "ncbiGeneId" = c(
-            7105L,
-            7982L,
-            10838L,
-            64102L
-        ),
-        "ensemblGeneId" = c(
-            "ENSG00000000003",
-            "ENSG00000004866",
-            "ENSG00000063587",
-            "ENSG00000000005"
-        )
-    )
-    expect_identical(
-        object = as.data.frame(object),
-        expected = as.data.frame(expected)
-    )
+    expect_identical(metadata(object)[["format"]], "1:1")
 })
 
 test_that("Invalid key", {
