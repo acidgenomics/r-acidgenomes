7a50a544e27ef0d73678eb0cb658069583042b7e
Michael Steinbaugh <mike@steinbaugh.com>
2023-11-27 15:52:47 -0500

    Draft update on match failure

--- longtests/testthat/test-NcbiToEnsembl.R
+++ longtests/testthat/test-NcbiToEnsembl.R
@@ -1,5 +1,8 @@
-## Entrez identifiers that multimap:
+## Multi-mapped identifiers that we can resolve:
 ## 2 16 19 22 23 25 28 29 31 34
+
+## FIXME 9410 match failures: "11", "17", "62", "63", "64", "66",
+"67", "68", "69", "73"....
 
 test_that("character : all HGNC genes", {
     hgnc <- Hgnc()
