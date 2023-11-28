8b68a00e23ae58a46359a5a4e54ac4a7b233df8d
Michael Steinbaugh <mike@steinbaugh.com>
2023-11-28 15:40:24 -0500

    Update checks

--- longtests/testthat/test-JaxHumanToMouse.R
+++ longtests/testthat/test-JaxHumanToMouse.R
@@ -1,7 +1,7 @@
 test_that("1:1 unique mapping", {
     object <- JaxHumanToMouse(unique = TRUE)
     expect_s4_class(object, "JaxHumanToMouse")
-    expect_identical(nrow(object), 16985L)
+    expect_identical(nrow(object), 16983L)
     expect_true(hasNoDuplicates(object[["humanGeneName"]]))
     expect_true(hasNoDuplicates(object[["mouseGeneName"]]))
 })
@@ -9,7 +9,7 @@ test_that("1:1 unique mapping", {
 test_that("Allow duplicates", {
     object <- JaxHumanToMouse(unique = FALSE)
     expect_s4_class(object, "JaxHumanToMouse")
-    expect_identical(nrow(object), 24600L)
+    expect_identical(nrow(object), 24601L)
     expect_false(hasNoDuplicates(object[["humanGeneName"]]))
     expect_false(hasNoDuplicates(object[["mouseGeneName"]]))
 })
