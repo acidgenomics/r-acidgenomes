bb32dff8290c40ed95156227aba4f0c0aa71ab11
Michael Steinbaugh <mike@steinbaugh.com>
2023-11-28 11:10:21 -0500

    Improve code coverage

--- tests/testthat/test-EnsemblToNcbi.R
+++ tests/testthat/test-EnsemblToNcbi.R
@@ -1,5 +1,3 @@
-## FIXME Need to cover Homo sapiens, Mus musculus, C. elegans, and Dmel.
-
 test_that("character : Homo sapiens", {
     genes <- c(
         "ENSG00000063587.15",
@@ -37,7 +35,39 @@ test_that("character : Homo sapiens", {
 })
 
 test_that("character : Mus musculus", {
-    genes <- c("FIXME")
+    genes <- c(
+        "ENSG00000063587.15",
+        "ENSG00000004866.22",
+        "ENSG00000000005.6",
+        "ENSG00000000003.16"
+    )
+    object <- EnsemblToNcbi(genes, organism = NULL)
+    expected <- DataFrame(
+        "ensemblGeneId" = c(
+            "ENSG00000063587",
+            "ENSG00000004866",
+            "ENSG00000000005",
+            "ENSG00000000003"
+        ),
+        "ncbiGeneId" = c(
+            10838L,
+            7982L,
+            64102L,
+            7105L
+        ),
+        row.names = genes
+    )
+    expect_identical(
+        object = as.data.frame(object),
+        expected = as.data.frame(expected)
+    )
+    expect_error(
+        object = EnsemblToNcbi(
+            object = "ENSG00000000000",
+            organism = "Homo sapiens"
+        ),
+        regexp = "match failure"
+    )
 })
 
 test_that("character : Caenorhabditis elegans", {
@@ -196,10 +226,6 @@ test_that("EnsemblGenes and Hgnc mapping consistency", {
 })
 
 test_that("EnsemblGenes and OrgDb consistency", {
-    stop("FIXME")
-})
-
-test_that("Hgnc and OrgDb consistency", {
     stop("FIXME")
 })
 
