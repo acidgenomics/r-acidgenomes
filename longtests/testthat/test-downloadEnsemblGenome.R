bc897224e80b22d59b7814e4f6116e6f600b0d0a
Michael Steinbaugh <mike@steinbaugh.com>
2023-12-04 11:51:54 -0500

    Work on improving genome code coverage

--- longtests/testthat/test-downloadEnsemblGenome.R
+++ longtests/testthat/test-downloadEnsemblGenome.R
@@ -1,12 +1,9 @@
-## FIXME Add coverage for GRCh37.
-## FIXME Add coverage for mouse genome.
-
-test_that("downloadEnsemblGenome", {
+test_that("Homo sapiens GRCh38", {
     testdir <- tempdir2()
     info <- downloadEnsemblGenome(
         organism = "Homo sapiens",
         genomeBuild = "GRCh38",
-        release = 108L,
+        release = 110L,
         outputDir = testdir,
         cache = TRUE
     )
@@ -28,18 +25,18 @@ test_that("downloadEnsemblGenome", {
     ))))
     genes <- import(file.path(outputDir, "genes.rds"))
     expect_s4_class(genes, "EnsemblGenes")
-    expect_length(genes, 62703L)
+    expect_length(genes, 62754L)
     expect_identical(
         object = head(sort(names(genes)), n = 3L),
         expected = c(
-            "ENSG00000000003.15",
+            "ENSG00000000003.16",
             "ENSG00000000005.6",
             "ENSG00000000419.14"
         )
     )
     transcripts <- import(file.path(outputDir, "transcripts.rds"))
     expect_s4_class(transcripts, "EnsemblTranscripts")
-    expect_length(transcripts, 252301L)
+    expect_length(transcripts, 252894L)
     expect_identical(
         object = head(sort(names(transcripts)), n = 3L),
         expected = c(
@@ -50,7 +47,7 @@ test_that("downloadEnsemblGenome", {
     )
     t2g <- import(file.path(outputDir, "tx2gene.rds"))
     expect_s4_class(t2g, "TxToGene")
-    expect_identical(nrow(t2g), 272929L)
+    expect_identical(nrow(t2g), 275741L)
     expect_identical(
         object = as.data.frame(t2g)[1L, , drop = TRUE],
         expected = list(
@@ -62,7 +59,7 @@ test_that("downloadEnsemblGenome", {
         con = file.path(outputDir, "tx2gene.csv.gz"),
         colnames = c("txId", "geneId")
     )
-    expect_identical(nrow(t2g), 272929L)
+    expect_identical(nrow(t2g), 275741L)
     expect_identical(
         object = as.data.frame(t2g)[1L, , drop = TRUE],
         expected = list(
@@ -72,3 +69,77 @@ test_that("downloadEnsemblGenome", {
     )
     unlink2(testdir)
 })
+
+test_that("Homo sapiens GRCh37", {
+    testdir <- tempdir2()
+    info <- downloadEnsemblGenome(
+        organism = "Homo sapiens",
+        genomeBuild = "GRCh37",
+        outputDir = testdir,
+        cache = TRUE
+    )
+    outputDir <- info[["args"]][["outputDir"]]
+    expect_true(dir.exists(outputDir))
+    expect_true(all(file.exists(file.path(
+        outputDir,
+        c(
+            "annotation.gff3.gz",
+            "annotation.gtf.gz",
+            "genes.rds",
+            "genome.fa.gz",
+            "metadata.rds",
+            "transcriptome.fa.gz",
+            "transcripts.rds",
+            "tx2gene.csv.gz",
+            "tx2gene.rds"
+        )
+    ))))
+    genes <- import(file.path(outputDir, "genes.rds"))
+    expect_s4_class(genes, "EnsemblGenes")
+    expect_length(genes, 62754L)
+    expect_identical(
+        object = head(sort(names(genes)), n = 3L),
+        expected = c(
+            "ENSG00000000003.16",
+            "ENSG00000000005.6",
+            "ENSG00000000419.14"
+        )
+    )
+    transcripts <- import(file.path(outputDir, "transcripts.rds"))
+    expect_s4_class(transcripts, "EnsemblTranscripts")
+    expect_length(transcripts, 252894L)
+    expect_identical(
+        object = head(sort(names(transcripts)), n = 3L),
+        expected = c(
+            "ENST00000000233.10",
+            "ENST00000000412.8",
+            "ENST00000000442.11"
+        )
+    )
+    t2g <- import(file.path(outputDir, "tx2gene.rds"))
+    expect_s4_class(t2g, "TxToGene")
+    expect_identical(nrow(t2g), 275741L)
+    expect_identical(
+        object = as.data.frame(t2g)[1L, , drop = TRUE],
+        expected = list(
+            "txId" = "ENST00000000233.10",
+            "geneId" = "ENSG00000004059.11"
+        )
+    )
+    t2g <- import(
+        con = file.path(outputDir, "tx2gene.csv.gz"),
+        colnames = c("txId", "geneId")
+    )
+    expect_identical(nrow(t2g), 275741L)
+    expect_identical(
+        object = as.data.frame(t2g)[1L, , drop = TRUE],
+        expected = list(
+            "txId" = "ENST00000000233.10",
+            "geneId" = "ENSG00000004059.11"
+        )
+    )
+    unlink2(testdir)
+})
+
+## FIXME Add coverage for GRCh37.
+## FIXME Add coverage for mouse genome.
