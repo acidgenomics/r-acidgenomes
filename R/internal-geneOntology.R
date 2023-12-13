4e08fe201c53361bb74e90b0827a0e123d6c3091
Michael Steinbaugh <mike@steinbaugh.com>
2023-12-13 11:58:56 -0500

    Add internal .mapGoTerms function

---
+++ R/internal-geneOntology.R
@@ -0,0 +1,24 @@
+.mapGoTerms <- function() {
+    url <- pasteUrl(
+        "purl.obolibrary.org",
+        "obo",
+        "go",
+        "go-basic.obo",
+        protocol = "https"
+    )
+    obo <- import(con = .cacheIt(url), format = "obo")
+    df <- as.data.frame(obo)
+    assert(isSubset(c("id", "name", "obsolete"), colnames(df)))
+    i <- !df[["obsolete"]]
+    j <- c("id", "name")
+    df <- df[i, j, drop = FALSE]
+    df <- as(df, "DFrame")
+    assert(hasNoDuplicates(df[["id"]]))
+    df <- sort(df)
+    rownames(df) <- NULL
+    metadata(df) <- list(
+        "date" = Sys.Date(),
+        "url" = url
+    )
+    df
+}
