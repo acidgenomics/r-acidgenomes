358b948b26e516fdb3227ced760f441253b925ec
Michael Steinbaugh <mike@steinbaugh.com>
2023-12-13 06:58:19 -0500

    Add `mapRnacentral` function

---
+++ R/mapRnacentral.R
@@ -0,0 +1,77 @@
+#' Import RNAcentral identifier mappings
+#'
+#' @export
+#' @note Updated 2023-12-13.
+#'
+#' @inheritParams AcidRoxygen::params
+#'
+#' @param database `character(1)`.
+#' Database name. Currently supported: `"ensembl"`, `"ensembl_gencode"`,
+#' `"hgnc"`, `"mgi"`, and `"refseq"`. HGNC database is *Homo sapiens* specific,
+#' and MGI is *Mus musculus* specific.
+#'
+#' @return `DFrame`.
+#' Contains RNAcentral to specified database RNA transcript identifier mappings.
+#' Returns `"rnacentralId"`, `"database"`, `"txId"`, `"taxId"`, `"txBiotype"`,
+#' and `"geneId"` columns.
+#'
+#' @seealso
+#' - https://rnacentral.org/
+#' - https://ftp.ebi.ac.uk/pub/databases/RNAcentral/
+#'
+#' @examples
+#' ## Get RNAcentral to RefSeq identifier mappings.
+#' df <- mapRnacentral(organism = "Homo sapiens", database = "refseq")
+mapRnacentral <- function(organism, database) {
+    assert(isOrganism(organism))
+    database <- match.arg(
+        arg = database,
+        choices = c(
+            "ensembl",
+            "ensembl_gencode",
+            "hgnc",
+            "mgi",
+            "refseq"
+        )
+    )
+    taxId <- .mapOrganismToNcbiTaxId(organism)
+    url <- pasteUrl(
+        "ftp.ebi.ac.uk",
+        "pub",
+        "databases",
+        "RNAcentral",
+        "current_release",
+        "id_mapping",
+        "database_mappings",
+        paste0(database, ".tsv"),
+        protocol = "ftp"
+    )
+    df <- import(
+        con = .cacheIt(url),
+        colnames = c(
+            "rnacentralId",
+            "database",
+            "txId",
+            "taxId",
+            "txBiotype",
+            "geneId"
+        )
+    )
+    assert(
+        isSubset(taxId, unique(df[["taxId"]])),
+        msg = sprintf(
+            "{.var %s} ({.var %s}) not defined in {.url %s}.",
+            organism, taxId, url
+        )
+    )
+    i <- df[["taxId"]] == taxId
+    df <- df[i, , drop = FALSE]
+    df <- as(df, "DFrame")
+    metadata(df) <- list(
+        "database" = database,
+        "date" = Sys.Date(),
+        "organism" = organism,
+        "url" = url
+    )
+    df
+}
