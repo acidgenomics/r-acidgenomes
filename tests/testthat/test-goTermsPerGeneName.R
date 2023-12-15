acf13b6df4f2fa6fb129dde5b30cf344c8523348
Michael Steinbaugh <mike@steinbaugh.com>
2023-12-15 12:19:29 -0500

    Improve coverage

--- tests/testthat/test-goTermsPerGeneName.R
+++ tests/testthat/test-goTermsPerGeneName.R
@@ -18,8 +18,31 @@ test_that("Homo sapiens : long", {
 })
 
 test_that("Homo sapiens : split", {
-
+    object <- goTermsPerGeneName(
+        organism = "Homo sapiens",
+        geneNames = c("MTOR", "NFE2L2"),
+        format = "split"
+    )
+    expect_s4_class(object, "SplitDFrameList")
+    expect_length(object, 2L)
+    expect_named(object, c("MTOR", "NFE2L2"))
+    expect_identical(
+        object = as.data.frame(object[["MTOR"]][1L, ]),
+        expected = data.frame(
+            "geneName" = "MTOR",
+            "goCategory" = "BP",
+            "goId" = "GO:0001558",
+            "goName" = "regulation of cell growth"
+        )
+    )
 })
 
 test_that("Homo sapiens : nested", {
+    object <- goTermsPerGeneName(
+        organism = "Homo sapiens",
+        geneNames = c("MTOR", "NFE2L2"),
+        format = "nested"
+    )
+    expect_identical(nrow(object), 2L)
+    expect_null(rownames(object))
 })
