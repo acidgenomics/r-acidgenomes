f766e0afffcfa6c0cadc71cc142fc4210776fe07
Michael Steinbaugh <mike@steinbaugh.com>
2023-12-19 12:51:13 -0500

    Resolve FIXME

--- R/NcbiGeneInfo.R
+++ R/NcbiGeneInfo.R
@@ -1,9 +1,3 @@
-## FIXME Can we add gene ontology information?
-## http://current.geneontology.org/products/pages/downloads.html
-## http://geneontology.org/gene-associations/goa_human_rna.gaf.gz
-
-
-
 #' Import NCBI (Entrez) gene identifier information
 #'
 #' @export
