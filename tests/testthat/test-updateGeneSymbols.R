a24fbf5265f43ab876c8e3da64d51ef62e4ce0d5
Michael Steinbaugh <mike@steinbaugh.com>
2023-12-21 11:38:53 -0500

    Add updateGeneSymbols

---
+++ tests/testthat/test-updateGeneSymbols.R
@@ -0,0 +1,27 @@
+test_that("Homo sapiens", {
+    object <- updateGeneSymbols(
+        geneNames = c("ZCCHC6", "ZCCHC11"),
+        organism = "Homo sapiens"
+    )
+    expect_identical(
+        object = object,
+        expected = c(
+            "ZCCHC6" = "TUT7",
+            "ZCCHC11" = "TUT4"
+        )
+    )
+})
+
+test_that("Mus musculus", {
+    object <- updateGeneSymbols(
+        geneNames = c("Zcchc6", "Zcchc11"),
+        organism = "Mus musculus"
+    )
+    expect_identical(
+        object = object,
+        expected = c(
+            "Zcchc6" = "Tut7",
+            "Zcchc11" = "Tut4"
+        )
+    )
+})
