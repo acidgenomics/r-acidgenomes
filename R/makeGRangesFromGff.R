034d379309b1aa2d9acc83054a9191466748cc78
Michael Steinbaugh <mike@steinbaugh.com>
2023-12-21 07:30:21 -0500

    Draft updates to GFF parsers

--- R/makeGRangesFromGff.R
+++ R/makeGRangesFromGff.R
@@ -9,7 +9,7 @@
 #' Make genomic ranges (`GRanges`) from a GFF/GTF file
 #'
 #' @export
-#' @note Updated 2023-12-20.
+#' @note Updated 2023-12-21.
 #'
 #' @details
 #' Remote URLs and compressed files are supported.
