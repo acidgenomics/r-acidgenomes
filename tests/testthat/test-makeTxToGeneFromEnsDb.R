bf5edfd8adc9858f82c7ecfee86b94b05c097d80
Michael Steinbaugh <mike@steinbaugh.com>
2023-12-21 10:37:46 -0500

    Add FIXME

--- tests/testthat/test-makeTxToGeneFromEnsDb.R
+++ tests/testthat/test-makeTxToGeneFromEnsDb.R
@@ -3,4 +3,5 @@ skip_if_not_installed("EnsDb.Hsapiens.v75")
 test_that("makeTxToGeneFromEnsDb", {
     object <- makeTxToGeneFromEnsDb("EnsDb.Hsapiens.v75")
     expect_s4_class(object, "TxToGene")
+    # FIXME Check the number of rows here.
 })
