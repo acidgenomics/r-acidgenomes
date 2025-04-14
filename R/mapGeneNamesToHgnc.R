c1e2fe75f584204d7b3d48129065a2f2a14d3970
Michael Steinbaugh <mike@steinbaugh.com>
2025-04-14 17:36:07 -0400

    Add support for case insensitive matching

--- R/mapGeneNamesToHgnc.R
+++ R/mapGeneNamesToHgnc.R
@@ -3,6 +3,8 @@
 #' @export
 #' @note Updated 2025-04-14.
 #'
+#' @inheritParams params
+#'
 #' @param genes `character`.
 #' Human gene names (e.g. `"TUT4"`).
 #'
@@ -13,47 +15,46 @@
 #' ## Homo sapiens (only).
 #' x <- mapGeneNamesToHgnc(genes = c("TUT4", "ZCCHC11", "TENT3A"))
 #' print(x)
-mapGeneNamesToHgnc <- function(genes, hgnc = NULL) {
-    if (is.null(hgnc)) {
-        hgnc <- Hgnc()
-    }
-    assert(
-        isCharacter(genes),
-        is(hgnc, "Hgnc"),
-        validObject(hgnc),
-        isSubset(
-            x = c("aliasSymbol", "geneName", "hgncId", "prevSymbol"),
-            y = colnames(hgnc)
-        )
-    )
-    table <- as(hgnc, "DFrame")
-    ## Enable case-insensitive matching.
-    table[["geneName2"]] <- toupper(table[["geneName"]])
-    table[["prevSymbol2"]] <- toupper(table[["prevSymbol"]])
-    table[["aliasSymbol2"]] <- toupper(table[["aliasSymbol"]])
-    cols <- c(
-        "geneName",
-        "prevSymbol",
-        "aliasSymbol",
-        "geneName2",
-        "prevSymbol2",
-        "aliasSymbol2"
-    )
-    table <- table[, cols]
-    idx <- matchNested(x = genes, table = table)
-    if (anyNA(idx)) {
-        fail <- genes[is.na(idx)]
-        abort(sprintf(
-            "%d mapping %s: %s.",
-            length(fail),
-            ngettext(
-                n = length(fail),
-                msg1 = "failure",
-                msg2 = "failures"
+mapGeneNamesToHgnc <-
+    function(genes,
+             ignoreCase = FALSE,
+             hgnc = NULL) {
+        if (is.null(hgnc)) {
+            hgnc <- Hgnc()
+        }
+        assert(
+            isCharacter(genes),
+            is(hgnc, "Hgnc"),
+            validObject(hgnc),
+            isSubset(
+                x = c("aliasSymbol", "geneName", "hgncId", "prevSymbol"),
+                y = colnames(hgnc)
             ),
-            toInlineString(x = fail, n = 20L)
-        ))
+            isFlag(ignoreCase)
+        )
+        table <- as(hgnc, "DFrame")
+        cols <- c("geneName", "prevSymbol", "aliasSymbol")
+        table <- table[, cols]
+        if (isTRUE(ignoreCase)) {
+            genes <- toupper(genes)
+            for (col in cols) {
+                table[[col]] <- toupper(table[[col]])
+            }
+        }
+        idx <- matchNested(x = genes, table = table)
+        if (anyNA(idx)) {
+            fail <- genes[is.na(idx)]
+            abort(sprintf(
+                "%d mapping %s: %s.",
+                length(fail),
+                ngettext(
+                    n = length(fail),
+                    msg1 = "failure",
+                    msg2 = "failures"
+                ),
+                toInlineString(x = fail, n = 20L)
+            ))
+        }
+        out <- hgnc[idx, "hgncId", drop = TRUE]
+        out
     }
-    out <- hgnc[idx, "hgncId", drop = TRUE]
-    out
-}
