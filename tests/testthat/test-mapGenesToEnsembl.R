ba011d95b7498737f70cdcdf6982167567315b95
Michael Steinbaugh <mike@steinbaugh.com>
2025-04-15 08:02:45 -0400

    Draft update reworking our mapping approach

--- tests/testthat/test-mapGenesToEnsembl.R
+++ tests/testthat/test-mapGenesToEnsembl.R
@@ -2,30 +2,17 @@ test_that("Homo sapiens", {
     expect_identical(
         object = mapGeneNamesToEnsembl(
             genes = c("TUT4", "ZCCHC11", "TENT3A"),
-            organism = "Homo sapiens",
-            genomeBuild = "GRCh38",
-            release = 109L
+            organism = "Homo sapiens"
         ),
         expected = rep("ENSG00000134744", times = 3L)
     )
 })
 
-## FIXME This test is now failing due to missing URL:
-## Seems like the URL has migrated from:
-## https://ftp.ncbi.nlm.nih.gov/gene/DATA/GENE_INFO/Mammalia/Mus_musculus.gene_info.gz
-## to:
-## https://ftp.ncbi.nlm.nih.gov/gene/DATA/GENE_INFO/Mammalia/Mus_musculus.gene_info.gz
-## Seems like the NCBI server is currently down.
-## goalie::isAnExistingUrl("https://ftp.ncbi.nlm.nih.gov")
-## ## goalie::isAnExistingUrl("https://ftp.ncbi.nlm.nih.gov")
-
 test_that("Mus musculus", {
     expect_identical(
         object = mapGeneNamesToEnsembl(
             genes = c("Nfe2l2", "Nrf2"),
-            organism = "Mus musculus",
-            genomeBuild = "GRCm39",
-            release = 109L
+            organism = "Mus musculus"
         ),
         expected = rep("ENSMUSG00000015839", times = 2L)
     )
